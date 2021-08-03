499e8a912242b62dc19c39d555664d34ad6156a6
Max Nusspickel <max.nusspickel@googlemail.com>
2021-08-03 13:13:42 +0100

    Improvements to Bethe functions.

--- vayesta/lattmod/bethe.py
+++ vayesta/lattmod/bethe.py
@@ -3,15 +3,38 @@ import scipy
 import scipy.integrate
 
 
-def hubbard1d_bethe_energy(t, u, interval=(1e-14, 100), **kwargs):
-    """Exact total energy per site for the 1D Hubbard model.
+def hubbard1d_bethe_energy(t, u, interval=(1e-14, 30.75*np.pi), **kwargs):
+    """Exact total energy per site for the 1D Hubbard model in the thermodynamic limit.
 
     from DOI: 10.1103/PhysRevB.77.045133."""
+
+    kwargs['limit'] = kwargs.get('limit', 100)
 
     def func(x):
         j0 = scipy.special.jv(0, x)
         j1 = scipy.special.jv(1, x)
-        f = j0*j1/(x*(1 + np.exp((x*u)/(2*t))))
+        eu = np.exp((x*u)/(2*t))
+        f = j0*j1/(x*(1 + eu))
+        return f
+
+    e, *res = scipy.integrate.quad(func, *interval, **kwargs)
+    e = -4*t*e
+
+    return e
+
+def hubbard1d_bethe_docc(t, u, interval=(1e-14, 30.75*np.pi), **kwargs):
+    """Exact on-site double occupancy for the 1D Hubbard model in the thermodynamic limit."""
+
+    kwargs['limit'] = kwargs.get('limit', 100)
+
+    def func(x):
+        j0 = scipy.special.jv(0, x)
+        j1 = scipy.special.jv(1, x)
+        eu = np.exp((x*u)/(2*t))
+        #f = j0*j1/x * (-eu)/(1+eu)**2 * x/(2*t)
+        # Avoid float overflow:
+        emu = np.exp(-(x*u)/(2*t))
+        f = j0*j1/x * (-1)/(2+emu+eu) * x/(2*t)
         return f
 
     e, *res = scipy.integrate.quad(func, *interval, **kwargs)
@@ -20,8 +43,8 @@ def hubbard1d_bethe_energy(t, u, interval=(1e-14, 100), **kwargs):
     return e
 
 
-def hubbard1d_bethe_double_occ(t, u, du=1e-10, order=2, **kwargs):
-    """Exact on-site double occupancy for the 1D Hubbard model.
+def hubbard1d_bethe_docc_numdiff(t, u, du=1e-10, order=2, **kwargs):
+    """Exact on-site double occupancy for the 1D Hubbard model in the thermodynamic limit.
 
     Calculated from the numerical differentiation of the exact Bethe ansatz energy."""
 
@@ -42,8 +65,7 @@ def hubbard1d_bethe_double_occ(t, u, du=1e-10, order=2, **kwargs):
 
 if __name__ == '__main__':
     t = 1.0
-    u = 10.0
-
-    e = hubbard1d_bethe_energy(t, u)
-    d = hubbard1d_bethe_double_occ(t, u)
-    print("t= %.3f U= %.3f : E(Bethe)= %.8f D(Bethe)= %.8f" % (t, u, e, d))
+    for u in range(0, 13):
+        e = hubbard1d_bethe_energy(t, u)
+        d = hubbard1d_bethe_docc(t, u)
+        print("U= %6.3f:  Energy= %.8f  Double occupancy= %.8f" % (u, e, d))
