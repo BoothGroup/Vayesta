8a3991e669d9550f54e85eb93d657abff04aecb3
Ollie Backhouse <olbackhouse@gmail.com>
2021-09-07 13:09:53 +0100

    More molecular DMET tests

--- tests/dmet/test_dmet_molecular.py
+++ tests/dmet/test_dmet_molecular.py
@@ -5,7 +5,7 @@ import pyscf.gto
 import pyscf.scf
 import pyscf.tools.ring
 
-def make_test_molecular(atoms, basis, solver, fragment_type, cc, known_values, fragments = None, maxiter = 20):
+def make_test_molecular(atoms, basis, solver, fragment_type, cc, known_values, fragments=None, **kwargs):
 
     class DMETMolecularTests(unittest.TestCase):
 
@@ -19,7 +19,7 @@ def make_test_molecular(atoms, basis, solver, fragment_type, cc, known_values, f
             cls.mf = pyscf.scf.RHF(cls.mol)
             cls.mf.kernel()
 
-            cls.dmet = dmet.DMET(cls.mf, solver=solver, fragment_type=fragment_type, charge_consistent = cc, maxiter=20)
+            cls.dmet = dmet.DMET(cls.mf, solver=solver, charge_consistent=cc, **kwargs)
             # Ensure that we don't spam with output.
             cls.dmet.log.setLevel(50)
             if fragments is None:
@@ -38,18 +38,59 @@ def make_test_molecular(atoms, basis, solver, fragment_type, cc, known_values, f
 
     return DMETMolecularTests
 
-def make_test_Hring(natom, d, fragsize, *args, maxiter = 20):
+def make_test_Hring(natom, d, fragsize, *args, **kwargs):
     ring = pyscf.tools.ring.make(natom, d)
     atom = [('H %f %f %f' % xyz) for xyz in ring]
     fragments = [list(range(x, x+fragsize)) for x in range(0, natom, fragsize)]
-    return make_test_molecular(atom, *args, fragments = fragments, maxiter = maxiter)
+    return make_test_molecular(atom, *args, fragments=fragments, **kwargs)
 
 
-dmet_Hring_sto6g_FCI_IAO_cc_Test = make_test_Hring(10, 1.0, 2, "sto-6g", "FCI", "IAO", True,
-                                            {'e_tot': -5.421103410208376})
+dmet_Hring_sto6g_FCI_IAO_cc_Test = make_test_Hring(
+        10,
+        1.0,
+        2,
+        "sto-6g",
+        "FCI",
+        "IAO",
+        True,
+        {'e_tot': -5.421103410208376},
+)
 
-dmet_Hring_sto6g_FCI_IAO_nocc_Test = make_test_Hring(10, 1.0, 2, "sto-6g", "FCI", "IAO", False,
-                                            {'e_tot': -5.421192647967002})
+dmet_Hring_sto6g_FCI_IAO_nocc_Test = make_test_Hring(
+        10,
+        1.0,
+        2,
+        "sto-6g",
+        "FCI",
+        "IAO",
+        False,
+        {'e_tot': -5.421192647967002},
+)
+
+dmet_Hring_sto6g_FCI_IAO_all_nocc_Test = make_test_Hring(
+        10,
+        1.0,
+        2,
+        "sto-6g",
+        "FCI",
+        "IAO",
+        False,
+        {'e_tot': -5.422668582405825},
+        bath_type='ALL',
+)
+
+dmet_Hring_sto6g_FCI_IAO_BNO_nocc_Test = make_test_Hring(
+        10,
+        1.0,
+        2,
+        "sto-6g",
+        "FCI",
+        "IAO",
+        False,
+        {'e_tot': -5.421192648085972},
+        bath_type='MP2-BNO',
+        bno_threshold=np.inf,
+)
 
 
 if __name__ == '__main__':
