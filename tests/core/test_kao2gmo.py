0b6ee2a135f88c7f8f6af9f5dd53ea50570c8149
Max Nusspickel <max.nusspickel@googlemail.com>
2021-09-20 17:57:22 +0100

    Lower test threshold for kao2gmo

--- tests/core/test_kao2gmo.py
+++ tests/core/test_kao2gmo.py
@@ -9,6 +9,7 @@ from vayesta.misc import gdf
 #TODO: 2d systems with negative part
 #TODO: python driver
 
+PLACES_ERIS = 8
 
 class KAO2GMOTests(unittest.TestCase):
 
@@ -24,7 +25,9 @@ class KAO2GMOTests(unittest.TestCase):
         cls.mf = scf.KRHF(cls.cell, cls.kpts)
         cls.mf.with_df = gdf.GDF(cls.cell, cls.kpts)
         cls.mf.with_df.build()
+        cls.mf.conv_tol = 1e-12
         cls.mf.kernel()
+        assert cls.mf.converged
         cls.gmf = tools.k2gamma.k2gamma(cls.mf)
         cls.gmf = cls.gmf.density_fit()
 
@@ -40,7 +43,7 @@ class KAO2GMOTests(unittest.TestCase):
         for key in eri0.keys():
             coeffs = tuple([mo_coeff_occ, mo_coeff_vir][k == 'v'] for k in key)
             eri1 = mf.with_df.ao2mo(coeffs, compact=False).reshape([c.shape[1] for c in coeffs])
-            self.assertAlmostEqual(np.max(np.abs(eri0[key]-eri1)), 0.0, 10)
+            self.assertAlmostEqual(np.max(np.abs(eri0[key]-eri1)), 0.0, PLACES_ERIS)
 
     def test_mp2_eris(self):
         mf = self.gmf
@@ -51,7 +54,7 @@ class KAO2GMOTests(unittest.TestCase):
         for key in ['ovov']:
             coeffs = tuple([mo_coeff_occ, mo_coeff_vir][k == 'v'] for k in key)
             eri1 = mf.with_df.ao2mo(coeffs, compact=False).reshape([c.shape[1] for c in coeffs])
-            self.assertAlmostEqual(np.max(np.abs(getattr(eri0, key)-eri1)), 0.0, 10)
+            self.assertAlmostEqual(np.max(np.abs(getattr(eri0, key)-eri1)), 0.0, PLACES_ERIS)
 
     def test_rccsd_eris(self):
         mf = self.gmf
@@ -62,7 +65,7 @@ class KAO2GMOTests(unittest.TestCase):
         for key in ['ovov', 'oovv', 'ovvo', 'ovoo', 'oooo', 'ovvv', 'vvvv']:
             coeffs = tuple([mo_coeff_occ, mo_coeff_vir][k == 'v'] for k in key)
             eri1 = mf.with_df.ao2mo(coeffs, compact=False).reshape([c.shape[1] for c in coeffs])
-            self.assertAlmostEqual(np.max(np.abs(getattr(eri0, key)-eri1)), 0.0, 10)
+            self.assertAlmostEqual(np.max(np.abs(getattr(eri0, key)-eri1)), 0.0, PLACES_ERIS)
 
     def test_dfccsd_eris(self):
         mf = self.gmf
@@ -73,17 +76,17 @@ class KAO2GMOTests(unittest.TestCase):
         for key in ['ovov', 'oovv', 'ovvo', 'ovoo', 'oooo']:
             coeffs = tuple([mo_coeff_occ, mo_coeff_vir][k == 'v'] for k in key)
             eri1 = mf.with_df.ao2mo(coeffs, compact=False).reshape([c.shape[1] for c in coeffs])
-            self.assertAlmostEqual(np.max(np.abs(getattr(eri0, key)-eri1)), 0.0, 10)
+            self.assertAlmostEqual(np.max(np.abs(getattr(eri0, key)-eri1)), 0.0, PLACES_ERIS)
         # ovvv is compressed with sym=True:
         coeffs = [mo_coeff_occ, mo_coeff_vir, mo_coeff_vir, mo_coeff_vir]
         eri1 = mf.with_df.ao2mo(coeffs, compact=True)
         eri1 = eri1.reshape([coeffs[0].shape[1], coeffs[1].shape[1], -1])
-        self.assertAlmostEqual(np.max(np.abs(eri0.ovvv-eri1)), 0.0, 10)
+        self.assertAlmostEqual(np.max(np.abs(eri0.ovvv-eri1)), 0.0, PLACES_ERIS)
         # (vv|L)(L|vv) -> (vv|vv) contraction is performed late:
         coeffs = [mo_coeff_vir, mo_coeff_vir, mo_coeff_vir, mo_coeff_vir]
         eri1 = mf.with_df.ao2mo(coeffs, compact=True)
         eri0_vvvv = np.dot(eri0.vvL, eri0.vvL.T)
-        self.assertAlmostEqual(np.max(np.abs(eri0_vvvv-eri1)), 0.0, 10)
+        self.assertAlmostEqual(np.max(np.abs(eri0_vvvv-eri1)), 0.0, PLACES_ERIS)
 
     def test_ccsd_eris(self):
         mf = self.gmf
@@ -94,15 +97,15 @@ class KAO2GMOTests(unittest.TestCase):
         for key in ['ovov', 'oovv', 'ovvo', 'ovoo', 'oooo']:
             coeffs = tuple([mo_coeff_occ, mo_coeff_vir][k == 'v'] for k in key)
             eri1 = mf.with_df.ao2mo(coeffs, compact=False).reshape([c.shape[1] for c in coeffs])
-            self.assertAlmostEqual(np.max(np.abs(getattr(eri0, key)-eri1)), 0.0, 10)
+            self.assertAlmostEqual(np.max(np.abs(getattr(eri0, key)-eri1)), 0.0, PLACES_ERIS)
         # ovvv and vvvv are compressed with sym=True:
         coeffs = [mo_coeff_occ, mo_coeff_vir, mo_coeff_vir, mo_coeff_vir]
         eri1 = mf.with_df.ao2mo(coeffs, compact=True)
         eri1 = eri1.reshape([coeffs[0].shape[1], coeffs[1].shape[1], -1])
-        self.assertAlmostEqual(np.max(np.abs(eri0.ovvv-eri1)), 0.0, 10)
+        self.assertAlmostEqual(np.max(np.abs(eri0.ovvv-eri1)), 0.0, PLACES_ERIS)
         coeffs = [mo_coeff_vir, mo_coeff_vir, mo_coeff_vir, mo_coeff_vir]
         eri1 = mf.with_df.ao2mo(coeffs, compact=True)
-        self.assertAlmostEqual(np.max(np.abs(eri0.vvvv-eri1)), 0.0, 10)
+        self.assertAlmostEqual(np.max(np.abs(eri0.vvvv-eri1)), 0.0, PLACES_ERIS)
 
 
 if __name__ == '__main__':
