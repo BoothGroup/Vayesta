Fixes calculating T-sym fragments in EWF and adds orbital sign convention

examples/scmf/11-hubbard-1d-pdmet.py
@@ -27,5 +27,5 @@ for hubbard_u in range(0, 13):
 
     fmt = '%4.1f' + 3*'  %+16.8f' + '\n'
     energies = np.asarray([rhf.e_tot, rdmet.e_tot, e_rdmet]) / nelectron
-    with open("energies-imp-%d.txt", 'a') as f:
+    with open("energies-imp-%d.txt" % nimp, 'a') as f:
         f.write(fmt % (hubbard_u, *energies))

examples/scmf/12-hubbard-1d-pdmet-uhf.py
@@ -41,5 +41,5 @@ for hubbard_u in range(0, 13):
 
     fmt = '%4.1f' + 3*'  %+16.8f' + '\n'
     energies = np.asarray([uhf.e_tot, udmet.e_tot, e_udmet]) / nelectron
-    with open("energies-imp-%d-uhf.txt", 'a') as f:
+    with open("energies-imp-%d-uhf.txt" % nimp, 'a') as f:
         f.write(fmt % (hubbard_u, *energies))

vayesta/core/qemb/fragment.py
@@ -12,7 +12,7 @@ import pyscf.lib
 import pyscf.lo
 
 from vayesta.core.util import *
-from vayesta.core import helper, tsymmetry
+from vayesta.core import tsymmetry
 import vayesta.core.ao2mo
 import vayesta.core.ao2mo.helper
 from vayesta.core.bath import DMET_Bath
@@ -205,7 +205,7 @@ class Fragment:
                 self.id, self.name, self.n_frag, self.nelectron, self.sym_factor)
 
     def __str__(self):
-        return '%s %d: %s' % (self.__class__.__name__, self.id, self.trimmed_name())
+        return '%s %d: %s' % (self.__class__.__name__, self.id, self.name)
 
     def log_info(self):
         # Some output
@@ -485,7 +485,7 @@ class Fragment:
         mo_energy, rot = np.linalg.eigh(fock)
         mo_can = np.dot(mo_coeff, rot)
         if sign_convention:
-            mo_can, signs = helper.orbital_sign_convention(mo_can)
+            mo_can, signs = fix_orbital_sign(mo_can)
             rot = rot*signs[np.newaxis]
         assert np.allclose(np.dot(mo_coeff, rot), mo_can)
         assert np.allclose(np.dot(mo_can, rot.T), mo_coeff)
@@ -523,6 +523,7 @@ class Fragment:
             raise RuntimeError("Eigenvalues of cluster-DM not all close to 0 or %d:\n%s" % (e, norm))
         e, r = e[::-1], r[:,::-1]
         c_cluster = np.dot(c_cluster, r)
+        c_cluster = fix_orbital_sign(c_cluster)[0]
         nocc = np.count_nonzero(e >= (norm/2))
         c_cluster_occ, c_cluster_vir = np.hsplit(c_cluster, [nocc])
         return c_cluster_occ, c_cluster_vir