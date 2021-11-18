0c68ea3ad3cdbe4fbec195a6f4006732df3d63c2
Ollie <olbackhouse@gmail.com>
2021-11-18 17:08:30 +0000

    Adds codestyle check to tests script

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,12 @@
 '''
-Run tests an produce reports on timings and coverage.
-    - To view the coverage report: `firefox htmlcov/index.html`
-    - To view the profile report: `snakeviz .profile`
+Runs unit tests, and do the following:
+ ⋅ Produce a coverage report (.coverage)
+ ⋅ Produce a profile report (.profile)
+ ⋅ Produce a codestyle report (.codestyle)
+For post-processing:
+ ⋅ `firefox htmlcov/index.html`
+ ⋅ `snakeviz .profile`
+ ⋅ `vi .codestyle`
 '''
 
 # Imports:
@@ -12,21 +17,35 @@ import unittest
 import coverage
 import cProfile
 import logging
+import pycodestyle
+import io
+import contextlib
 
 # Clean up:
 os.system('rm -f .coverage')
 os.system('rm -f .profile')
+os.system('rm -f .codestyle')
 os.system('rm -rf htmlcov')
 
 # If only clean required, exit here:
 if len(sys.argv) > 1 and sys.argv[1] == 'clean':
     sys.exit(0)
 
+# Produce the codestyle report:
+style = pycodestyle.StyleGuide(paths=['vayesta/'], report=pycodestyle.FileReport)
+f = io.StringIO()
+with contextlib.redirect_stdout(f):
+    report = style.check_files()
+with open('.codestyle', 'w') as file:
+    file.write('\n'.join(report.get_statistics()))
+    file.write('\n\n')
+    file.write(f.getvalue())
+
 # Start the coverage report:
 cov = coverage.Coverage()
 cov.start()
 
-# Import Vayesta and restrict logging to WARNING:
+# Import Vayesta (inside coverage scope) and restrict logging to WARNING:
 import vayesta
 vayesta.log.setLevel(logging.WARNING)
 
