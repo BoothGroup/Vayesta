e90704dd178c3e072f05b7855528fe65d9ab0ffd
Ollie Backhouse <olbackhouse@gmail.com>
2021-11-20 12:57:32 +0000

    Unrestricted SCMF tests

--- tests/core/test_scmf.py
+++ tests/core/test_scmf.py
@@ -45,6 +45,48 @@ class SCMFTests(unittest.TestCase):
         self.assertAlmostEqual(emb.with_scmf.e_tot, -109.06844454464843, 8)
 
 
+class USCMFTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.mol = gto.Mole()
+        cls.mol.atom = 'Be 0 0 0; H 0 0 1'
+        cls.mol.basis = '6-31g'
+        cls.mol.spin = 1
+        cls.mol.verbose = 0
+        cls.mol.max_memory = 1e9
+        cls.mol.build()
+        cls.mf = scf.UHF(cls.mol)
+        cls.mf.conv_tol = 1e-12
+        cls.mf.kernel()
+
+    @classmethod
+    def tearDownClass(cls):
+        del cls.mol, cls.mf
+
+    def test_pdmet(self):
+        emb = ewf.EWF(self.mf, make_rdm1=True, bath_type=None)
+        emb.sao_fragmentation()
+        emb.make_all_atom_fragments()
+        emb.pdmet_scmf(etol=1e-10, dtol=1e-8)
+        emb.kernel()
+
+        self.assertTrue(emb.with_scmf.converged)
+        self.assertAlmostEqual(emb.with_scmf.e_tot_oneshot, -15.102484998592503, 8)
+        self.assertAlmostEqual(emb.with_scmf.e_tot, -15.102388080235999, 8)
+
+    def test_brueckner(self):
+        emb = ewf.EWF(self.mf, make_rdm1=True, bath_type=None)
+        emb.sao_fragmentation()
+        emb.make_all_atom_fragments()
+        emb.brueckner_scmf(etol=1e-10, dtol=1e-8)
+        emb.kernel()
+
+        self.assertTrue(emb.with_scmf.converged)
+        self.assertAlmostEqual(emb.with_scmf.e_tot_oneshot, -15.102484998592503, 8)
+        self.assertAlmostEqual(emb.with_scmf.e_tot, -15.10241716615656, 8)
+
+
 if __name__ == '__main__':
     print('Running %s' % __file__)
     unittest.main()
