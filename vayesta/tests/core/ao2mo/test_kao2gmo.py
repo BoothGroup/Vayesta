06bce7b1896a3f9a2758c5890895112e3768772e
Ollie Backhouse <olbackhouse@gmail.com>
2021-11-24 15:38:21 +0000

    Reformats test_kao2gmo.py

--- vayesta/tests/core/ao2mo/test_kao2gmo.py
+++ vayesta/tests/core/ao2mo/test_kao2gmo.py
@@ -1,76 +1,62 @@
 import unittest
 import numpy as np
-from pyscf import mp, cc
-from pyscf.pbc import gto, scf, tools
-from pyscf.cc import dfccsd
-from vayesta.core.ao2mo import kao2gmo
-from vayesta.misc import gdf
 
-#TODO: make sure that the example includes sufficient imaginary parts?
-#TODO: 2d systems with negative part
-#TODO: python driver
+from pyscf import mp, cc
+from pyscf.pbc import tools
+from pyscf.cc import dfccsd
+
+from vayesta.core.ao2mo import kao2gmo
+from vayesta.tests import cache
 
 PLACES_ERIS = 8
 
+
+#class KAO2GMO2dTests(unittest.TestCase):
+#
+#    key = 'h2_sto3g_331_2d'
+#
+#    def test_gdf_to_eris_2d(self):
+#        """Test agreement between unfolded and gamma-point integrals.
+#        """
+#
+#        mf = cache.cells[self.key]['rhf']
+#        gmf = tools.k2gamma.k2gamma(mf)
+#        gmf = gmf.density_fit()
+#        mo_coeff_occ = gmf.mo_coeff[:, gmf.mo_occ > 0]
+#        mo_coeff_vir = gmf.mo_coeff[:, gmf.mo_occ == 0]
+#
+#        eri0 = kao2gmo.gdf_to_eris(mf.with_df, gmf.mo_coeff, nocc=mo_coeff_occ.shape[1], only_ovov=False)
+#        eri1 = kao2gmo.gdf_to_eris(mf.with_df, gmf.mo_coeff, nocc=mo_coeff_occ.shape[1], only_ovov=False, real_j3c=False)
+#        for key in eri0.keys():
+#            coeffs = tuple([mo_coeff_occ, mo_coeff_vir][k == 'v'] for k in key)
+#            eri2 = gmf.with_df.ao2mo(coeffs, compact=False).reshape([c.shape[1] for c in coeffs])
+#            self.assertAlmostEqual(np.max(np.abs(eri0[key]-eri2)), 0.0, PLACES_ERIS)
+#            self.assertAlmostEqual(np.max(np.abs(eri1[key]-eri2)), 0.0, PLACES_ERIS)
+
+
 class KAO2GMOTests(unittest.TestCase):
+
+    key = 'he_631g_222'
 
     @classmethod
     def setUpClass(cls):
-        cls.cell = gto.Cell()
-        cls.cell.atom = 'He 0 0 0'
-        cls.cell.basis = '6-31g'
-        cls.cell.a = np.eye(3) * 3
-        cls.cell.verbose = 0
-        cls.cell.build()
-        cls.kpts = cls.cell.make_kpts([2,2,2])
-        cls.mf = scf.KRHF(cls.cell, cls.kpts)
-        cls.mf.with_df = gdf.GDF(cls.cell, cls.kpts)
-        cls.mf.with_df.build()
-        cls.mf.conv_tol = 1e-12
-        cls.mf.kernel()
-        assert cls.mf.converged
-        cls.gmf = tools.k2gamma.k2gamma(cls.mf)
+        cls.gmf = tools.k2gamma.k2gamma(cache.cells[cls.key]['rhf'])
         cls.gmf = cls.gmf.density_fit()
 
     @classmethod
     def tearDownClass(cls):
-        del cls.cell, cls.kpts, cls.mf
+        del cls.gmf
 
     def test_gdf_to_eris(self):
-        mf = self.gmf
-        mo_coeff_occ = mf.mo_coeff[:, mf.mo_occ > 0]
-        mo_coeff_vir = mf.mo_coeff[:, mf.mo_occ == 0]
+        """Test agreement between unfolded and gamma-point integrals.
+        """
 
-        eri0 = kao2gmo.gdf_to_eris(self.mf.with_df, mf.mo_coeff, nocc=mo_coeff_occ.shape[1], only_ovov=False, j3c_threshold=1e-12)
-        eri1 = kao2gmo.gdf_to_eris(self.mf.with_df, mf.mo_coeff, nocc=mo_coeff_occ.shape[1], only_ovov=False, real_j3c=False)
-        for key in eri0.keys():
-            coeffs = tuple([mo_coeff_occ, mo_coeff_vir][k == 'v'] for k in key)
-            eri2 = mf.with_df.ao2mo(coeffs, compact=False).reshape([c.shape[1] for c in coeffs])
-            self.assertAlmostEqual(np.max(np.abs(eri0[key]-eri2)), 0.0, PLACES_ERIS)
-            self.assertAlmostEqual(np.max(np.abs(eri1[key]-eri2)), 0.0, PLACES_ERIS)
-
-    def test_gdf_to_eris_2d(self):
-        L = 4.0
-        cell = gto.Cell()
-        cell.unit = 'B'
-        cell.atom = 'H 0 0 0; H 0 0 1.8'
-        cell.basis = 'sto3g'
-        cell.a = [[L, 0, 0], [0, L, 0], [0, 0, 30]]
-        cell.dimension = 2
-        cell.verbose = 0
-        cell.build()
-        kpts = cell.make_kpts([3, 3, 1])
-        mf = scf.KRHF(cell, kpts)
-        mf = mf.density_fit()
-        mf.conv_tol = 1e-12
-        mf.kernel()
-        assert mf.converged
-        gmf = tools.k2gamma.k2gamma(mf)
-        gmf = gmf.density_fit()
+        mf = cache.cells[self.key]['rhf']
+        gmf = self.gmf
         mo_coeff_occ = gmf.mo_coeff[:, gmf.mo_occ > 0]
         mo_coeff_vir = gmf.mo_coeff[:, gmf.mo_occ == 0]
 
-        eri0 = kao2gmo.gdf_to_eris(mf.with_df, gmf.mo_coeff, nocc=mo_coeff_occ.shape[1], only_ovov=False)
+        eri0 = kao2gmo.gdf_to_eris(mf.with_df, gmf.mo_coeff, nocc=mo_coeff_occ.shape[1], only_ovov=False, j3c_threshold=1e-12)
         eri1 = kao2gmo.gdf_to_eris(mf.with_df, gmf.mo_coeff, nocc=mo_coeff_occ.shape[1], only_ovov=False, real_j3c=False)
         for key in eri0.keys():
             coeffs = tuple([mo_coeff_occ, mo_coeff_vir][k == 'v'] for k in key)
@@ -79,66 +65,101 @@ class KAO2GMOTests(unittest.TestCase):
             self.assertAlmostEqual(np.max(np.abs(eri1[key]-eri2)), 0.0, PLACES_ERIS)
 
     def test_mp2_eris(self):
-        mf = self.gmf
-        cm = mp.mp2.MP2(mf)
-        mo_coeff_occ = mf.mo_coeff[:, mf.mo_occ > 0]
-        mo_coeff_vir = mf.mo_coeff[:, mf.mo_occ == 0]
-        eri0 = kao2gmo.gdf_to_pyscf_eris(mf, self.mf.with_df, cm, fock=mf.get_fock(), mo_energy=mf.mo_energy, e_hf=mf.e_tot)
+        """Test agreement between unfolded and gamma-point integrals
+        for the MP2 pyscf method.
+        """
+
+        mf = cache.cells[self.key]['rhf']
+        gmf = self.gmf
+        cm = mp.mp2.MP2(gmf)
+        mo_coeff_occ = gmf.mo_coeff[:, gmf.mo_occ > 0]
+        mo_coeff_vir = gmf.mo_coeff[:, gmf.mo_occ == 0]
+
+        eri0 = kao2gmo.gdf_to_pyscf_eris(gmf, mf.with_df, cm, fock=gmf.get_fock(), mo_energy=mf.mo_energy, e_hf=gmf.e_tot)
         for key in ['ovov']:
             coeffs = tuple([mo_coeff_occ, mo_coeff_vir][k == 'v'] for k in key)
-            eri1 = mf.with_df.ao2mo(coeffs, compact=False).reshape([c.shape[1] for c in coeffs])
+            eri1 = gmf.with_df.ao2mo(coeffs, compact=False).reshape([c.shape[1] for c in coeffs])
             self.assertAlmostEqual(np.max(np.abs(getattr(eri0, key)-eri1)), 0.0, PLACES_ERIS)
 
     def test_rccsd_eris(self):
-        mf = self.gmf
-        cm = cc.rccsd.RCCSD(mf)
-        mo_coeff_occ = mf.mo_coeff[:, mf.mo_occ > 0]
-        mo_coeff_vir = mf.mo_coeff[:, mf.mo_occ == 0]
-        eri0 = kao2gmo.gdf_to_pyscf_eris(mf, self.mf.with_df, cm, fock=mf.get_fock(), mo_energy=mf.mo_energy, e_hf=mf.e_tot)
+        """Test agreement between unfolded and gamma-point integrals
+        for the RCCSD pyscf method.
+        """
+
+        mf = cache.cells[self.key]['rhf']
+        gmf = self.gmf
+        cm = cc.rccsd.RCCSD(gmf)
+        mo_coeff_occ = gmf.mo_coeff[:, gmf.mo_occ > 0]
+        mo_coeff_vir = gmf.mo_coeff[:, gmf.mo_occ == 0]
+
+        eri0 = kao2gmo.gdf_to_pyscf_eris(gmf, mf.with_df, cm, fock=gmf.get_fock(), mo_energy=gmf.mo_energy, e_hf=gmf.e_tot)
         for key in ['ovov', 'oovv', 'ovvo', 'ovoo', 'oooo', 'ovvv', 'vvvv']:
             coeffs = tuple([mo_coeff_occ, mo_coeff_vir][k == 'v'] for k in key)
-            eri1 = mf.with_df.ao2mo(coeffs, compact=False).reshape([c.shape[1] for c in coeffs])
+            eri1 = gmf.with_df.ao2mo(coeffs, compact=False).reshape([c.shape[1] for c in coeffs])
             self.assertAlmostEqual(np.max(np.abs(getattr(eri0, key)-eri1)), 0.0, PLACES_ERIS)
 
     def test_dfccsd_eris(self):
-        mf = self.gmf
-        cm = dfccsd.RCCSD(mf)
-        mo_coeff_occ = mf.mo_coeff[:, mf.mo_occ > 0]
-        mo_coeff_vir = mf.mo_coeff[:, mf.mo_occ == 0]
-        eri0 = kao2gmo.gdf_to_pyscf_eris(mf, self.mf.with_df, cm, fock=mf.get_fock(), mo_energy=mf.mo_energy, e_hf=mf.e_tot)
+        """Test agreement between unfolded and gamma-point integrals
+        for the DFCCSD pyscf method.
+        """
+
+        mf = cache.cells[self.key]['rhf']
+        gmf = self.gmf
+        cm = dfccsd.RCCSD(gmf)
+        mo_coeff_occ = gmf.mo_coeff[:, gmf.mo_occ > 0]
+        mo_coeff_vir = gmf.mo_coeff[:, gmf.mo_occ == 0]
+
+        eri0 = kao2gmo.gdf_to_pyscf_eris(gmf, mf.with_df, cm, fock=gmf.get_fock(), mo_energy=gmf.mo_energy, e_hf=gmf.e_tot)
         for key in ['ovov', 'oovv', 'ovvo', 'ovoo', 'oooo']:
             coeffs = tuple([mo_coeff_occ, mo_coeff_vir][k == 'v'] for k in key)
-            eri1 = mf.with_df.ao2mo(coeffs, compact=False).reshape([c.shape[1] for c in coeffs])
+            eri1 = gmf.with_df.ao2mo(coeffs, compact=False).reshape([c.shape[1] for c in coeffs])
             self.assertAlmostEqual(np.max(np.abs(getattr(eri0, key)-eri1)), 0.0, PLACES_ERIS)
+
         # ovvv is compressed with sym=True:
         coeffs = [mo_coeff_occ, mo_coeff_vir, mo_coeff_vir, mo_coeff_vir]
-        eri1 = mf.with_df.ao2mo(coeffs, compact=True)
+        eri1 = gmf.with_df.ao2mo(coeffs, compact=True)
         eri1 = eri1.reshape([coeffs[0].shape[1], coeffs[1].shape[1], -1])
         self.assertAlmostEqual(np.max(np.abs(eri0.ovvv-eri1)), 0.0, PLACES_ERIS)
+
         # (vv|L)(L|vv) -> (vv|vv) contraction is performed late:
         coeffs = [mo_coeff_vir, mo_coeff_vir, mo_coeff_vir, mo_coeff_vir]
-        eri1 = mf.with_df.ao2mo(coeffs, compact=True)
+        eri1 = gmf.with_df.ao2mo(coeffs, compact=True)
         eri0_vvvv = np.dot(eri0.vvL, eri0.vvL.T)
         self.assertAlmostEqual(np.max(np.abs(eri0_vvvv-eri1)), 0.0, PLACES_ERIS)
 
     def test_ccsd_eris(self):
-        mf = self.gmf
-        cm = cc.ccsd.CCSD(mf)
-        mo_coeff_occ = mf.mo_coeff[:, mf.mo_occ > 0]
-        mo_coeff_vir = mf.mo_coeff[:, mf.mo_occ == 0]
-        eri0 = kao2gmo.gdf_to_pyscf_eris(mf, self.mf.with_df, cm, fock=mf.get_fock(), mo_energy=mf.mo_energy, e_hf=mf.e_tot)
+        """Test agreement between unfolded and gamma-point integrals
+        for the CCSD pyscf method.
+        """
+
+        mf = cache.cells[self.key]['rhf']
+        gmf = self.gmf
+        cm = cc.ccsd.CCSD(gmf)
+        mo_coeff_occ = gmf.mo_coeff[:, gmf.mo_occ > 0]
+        mo_coeff_vir = gmf.mo_coeff[:, gmf.mo_occ == 0]
+
+        eri0 = kao2gmo.gdf_to_pyscf_eris(gmf, mf.with_df, cm, fock=gmf.get_fock(), mo_energy=gmf.mo_energy, e_hf=gmf.e_tot)
         for key in ['ovov', 'oovv', 'ovvo', 'ovoo', 'oooo']:
             coeffs = tuple([mo_coeff_occ, mo_coeff_vir][k == 'v'] for k in key)
-            eri1 = mf.with_df.ao2mo(coeffs, compact=False).reshape([c.shape[1] for c in coeffs])
+            eri1 = gmf.with_df.ao2mo(coeffs, compact=False).reshape([c.shape[1] for c in coeffs])
             self.assertAlmostEqual(np.max(np.abs(getattr(eri0, key)-eri1)), 0.0, PLACES_ERIS)
+
         # ovvv and vvvv are compressed with sym=True:
         coeffs = [mo_coeff_occ, mo_coeff_vir, mo_coeff_vir, mo_coeff_vir]
-        eri1 = mf.with_df.ao2mo(coeffs, compact=True)
+        eri1 = gmf.with_df.ao2mo(coeffs, compact=True)
         eri1 = eri1.reshape([coeffs[0].shape[1], coeffs[1].shape[1], -1])
         self.assertAlmostEqual(np.max(np.abs(eri0.ovvv-eri1)), 0.0, PLACES_ERIS)
         coeffs = [mo_coeff_vir, mo_coeff_vir, mo_coeff_vir, mo_coeff_vir]
-        eri1 = mf.with_df.ao2mo(coeffs, compact=True)
+        eri1 = gmf.with_df.ao2mo(coeffs, compact=True)
         self.assertAlmostEqual(np.max(np.abs(eri0.vvvv-eri1)), 0.0, PLACES_ERIS)
+
+
+class KAO2GMO2dTests(KAO2GMOTests):
+    key = 'h2_sto3g_331_2d'
+
+    #FIXME
+    test_gdf_to_eris = None
+    test_dfccsd_eris = None
 
 
 if __name__ == '__main__':
