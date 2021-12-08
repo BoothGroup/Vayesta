Fix EDMET energy calculation.

vayesta/edmet/edmet.py
@@ -269,7 +269,7 @@ class EDMET(DMET):
             if exit:
                 break
             # Project rdm into fragment space; currently in cluster canonical orbitals.
-            c = dot(frag.c_frag.T, self.mf.get_ovlp(), frag.c_active)
+            c = dot(frag.c_frag.T, self.mf.get_ovlp(), frag.cluster.c_active)
             hl_rdms[x] = dot(c, frag.results.dm1, c.T)  # / 2
             nelec_hl += hl_rdms[x].trace() * nsym[x]
             # dd moments are already in fragment basis
@@ -289,8 +289,9 @@ class EDMET(DMET):
 
         def get_contrib(local_contrib, frag):
             (V_A_aa, V_A_ab, V_A_bb, V_B_aa, V_B_ab, V_B_bb) = local_contrib
-            c_occ = np.dot(self.get_ovlp(), frag.c_active_occ)
-            c_vir = np.dot(self.get_ovlp(), frag.c_active_vir)
+            c_occ = frag.get_fragment_projector()
+            c_occ = np.dot(self.get_ovlp(), frag.cluster.c_active_occ)
+            c_vir = np.dot(self.get_ovlp(), frag.cluster.c_active_vir)
             v_aa = (einsum("iajb,pi,qa,rj,sb->pqrs", V_A_aa, c_occ, c_vir, c_occ, c_vir) +
                     einsum("iajb,pi,qa,rj,sb->pqsr", V_B_aa, c_occ, c_vir, c_occ, c_vir))
             v_ab = (einsum("iajb,pi,qa,rj,sb->pqrs", V_A_ab, c_occ, c_vir, c_occ, c_vir) +

vayesta/edmet/fragment.py
@@ -269,12 +269,13 @@ class EDMETFragment(DMETFragment):
         e1, e2 = self.get_dmet_energy_contrib()
         c_act = self.cluster.c_active
         p_imp = self.get_fragment_projector(c_act)
-        # Taken spin-averaged couplings for now; should actually be spin symmetric.
-        couplings = (self._results.eb_couplings[0] + self._results.eb_couplings[1]) / 2
         dm_eb = self._results.dm_eb
+
         efb = 0.5 * (
-                np.einsum("pr,npq,rqn", p_imp, couplings, dm_eb) +
-                np.einsum("qr,npq,prn", p_imp, couplings, dm_eb)
+                np.einsum("pr,npq,rqn", p_imp, self.couplings[0], dm_eb[0]) +
+                np.einsum("qr,npq,prn", p_imp, self.couplings[0], dm_eb[0]) +
+                np.einsum("pr,npq,rqn", p_imp, self.couplings[1], dm_eb[1]) +
+                np.einsum("qr,npq,prn", p_imp, self.couplings[1], dm_eb[1])
         )
         return e1, e2, efb
 

vayesta/solver/eb_fci/ebfci_slow.py
@@ -3,6 +3,7 @@ Based on the fci_slow.py code within pyscf.
 """
 
 import numpy
+import numpy as np
 from pyscf import lib
 from pyscf import ao2mo
 from pyscf.fci import cistring
@@ -502,26 +503,28 @@ def make_eb_rdm(fcivec, norb, nelec, nbosons, max_occ):
         return numpy.zeros((nspinorb, nspinorb, 0))
 
     ci0 = fcivec.reshape(cishape)
-    t1 = numpy.zeros((nspinorb, nspinorb) + cishape, dtype=fcivec.dtype)
+    t1a = numpy.zeros((norb, norb) + cishape, dtype=fcivec.dtype)
+    t1b = numpy.zeros_like(t1a)
 
     for str0, tab in enumerate(link_indexa):
         for a, i, str1, sign in link_indexa[str0]:
-            t1[2 * a, 2 * i, str1] += sign * ci0[str0]
+            t1a[a, i, str1] += sign * ci0[str0]
     for str0, tab in enumerate(link_indexb):
         for a, i, str1, sign in link_indexb[str0]:
-            t1[2 * a + 1, 2 * i + 1, :, str1] += sign * ci0[:, str0]
+            t1b[a, i, :, str1] += sign * ci0[:, str0]
     bos_cre = numpy.sqrt(numpy.arange(1, max_occ + 1))
-    temp = numpy.zeros((nspinorb, nspinorb, nbosons) + ci0.shape)
+    tempa = numpy.zeros((norb, norb, nbosons) + ci0.shape)
+    tempb = np.zeros_like(tempa)
     for ibos in range(nbosons):
         # We could tidy this up with nicer slicing and einsum, but it works for now.
         for iocc in range(0, max_occ):
             ex_slice = (slice(None, None, None),) * 2 + (ibos,) + slices_for_cre(ibos, nbosons, iocc)
             norm_slice = (slice(None, None, None),) * 2 + slices_for(ibos, nbosons, iocc)
-            temp[ex_slice] += t1[norm_slice] * bos_cre[iocc]
-    rdm_fb = numpy.dot(
-        temp.reshape((nspinorb ** 2 * nbosons, -1)), ci0.reshape(-1)
-    ).reshape((nspinorb, nspinorb, nbosons))
-    return rdm_fb
+            tempa[ex_slice] += t1a[norm_slice] * bos_cre[iocc]
+            tempb[ex_slice] += t1b[norm_slice] * bos_cre[iocc]
+    rdm_fba = numpy.dot(tempa.reshape((norb ** 2 * nbosons, -1)), ci0.reshape(-1)).reshape((norb, norb, nbosons))
+    rdm_fbb = numpy.dot(tempb.reshape((norb ** 2 * nbosons, -1)), ci0.reshape(-1)).reshape((norb, norb, nbosons))
+    return rdm_fba, rdm_fbb
 
 
 def calc_dd_response_moment(ci0, e0, max_mom, norb, nel, nbos, h1e, eri, hbb, heb, max_boson_occ, rdm1, trace=False,

vayesta/solver/ebfci2.py
@@ -92,7 +92,7 @@ class EBFCI_Solver(FCI_Solver):
 
         self.dd_moms = ebfci_slow.calc_dd_response_moment_spatial(
             civec, self.e_fci, max_mom, self.ncas, self.nelec, self.nbos, heff, eris,
-            np.diag(self.fragment.bos_freqs), self.fragment.coupling, self.opts.max_boson_occ, dm1)
+            np.diag(self.fragment.bos_freqs), self.fragment.couplings, self.opts.max_boson_occ, dm1)
         return self.dd_moms
 
     def make_rdm_eb(self, civec=None):