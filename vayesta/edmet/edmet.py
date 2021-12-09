Fixed issue with numerical stability of matrix square root in numerical integration.

vayesta/edmet/edmet.py
@@ -205,7 +205,8 @@ class EDMET(DMET):
             rpa = ssRIRPA(self.mf, self.xc_kernel, self.log)
             # Get fermionic bath set up, and calculate the cluster excitation space.
             rot_ovs = [f.set_up_fermionic_bath() for f in sym_parents]
-            mom0_interact, est_error = rpa.kernel_moms(np.concatenate(rot_ovs, axis=0), npoints=48)
+            target_rot = np.concatenate(rot_ovs, axis=0)
+            mom0_interact, est_error = rpa.kernel_moms(target_rot, npoints=48)
             # Get appropriate slices to obtain required active spaces.
             ovs_active = [2*f.ov_active for f in sym_parents]
             ovs_active_slices = [slice(sum(ovs_active[:i]), sum(ovs_active[:i + 1])) for i in
@@ -232,7 +233,6 @@ class EDMET(DMET):
                 mom0_interact = dot(rot_ov, mom0)
                 rot_bos = f.define_bosons(mom0_interact)
                 mom0_bos = dot(rot_bos, mom0)
-                eps = get_eps()
                 f.construct_boson_hamil(mom0_bos, eps, self.xc_kernel)
 
     def calc_electron_number_defect(self, chempot, bno_thr, nelec_target, parent_fragments, nsym,

vayesta/edmet/fragment.py
@@ -76,9 +76,11 @@ class EDMETFragment(DMETFragment):
         """
         if rot_ov is None:
             rot_ov = self.get_rot_to_mf_ov()
+        self.rpa_mom = rpa_mom
         # Need to remove fermionic degrees of freedom from moment contribution. Null space of rotation matrix is size
         # N^4, so instead deduct projection onto fermionic space.
-        env_mom = rpa_mom - dot(rpa_mom, rot_ov.T, np.linalg.pinv(rot_ov.T))
+        rot_ov_pinv = np.linalg.pinv(rot_ov.T)
+        env_mom = rpa_mom - dot(rpa_mom, rot_ov.T, rot_ov_pinv)
         # v defines the rotation of the mean-field excitation space specifying our bosons.
         u, s, v = np.linalg.svd(env_mom, full_matrices=False)
         want = s > tol
@@ -88,7 +90,6 @@ class EDMETFragment(DMETFragment):
                           self.nbos, s[~want].max())
         else:
             self.log.info("Zeroth moment matching generated %d cluster bosons.", self.nbos)
-
         # Calculate the relevant components of the zeroth moment- we don't want to recalculate these.
         self.r_bos = v[want, :]
         self.eta0_ferm = np.dot(rpa_mom, rot_ov.T)
@@ -254,7 +255,6 @@ class EDMETFragment(DMETFragment):
         return results
 
     def get_solver_options(self, solver, chempot):
-        # TODO: fix this mess...
         solver_opts = {}
         solver_opts.update(self.opts.solver_options)
         pass_through = []

vayesta/rpa/rirpa/RIRPA.py
@@ -110,7 +110,6 @@ class ssRIRPA:
             niworker = momzero_NI.MomzeroDeductNone(*inputs)
             integral_offset = np.zeros_like(target_rot)
             moment_offset = np.zeros_like(target_rot)
-
         elif integral_deduct == "HO":
             niworker = momzero_NI.MomzeroDeductHigherOrder(*inputs)
             offset_niworker = momzero_NI.MomzeroOffsetCalcGaussLag(*inputs)
@@ -208,6 +207,11 @@ def construct_inverse_RI(D, ri):
     # This inversion and square root should only scale as O(N^3).
     U = np.linalg.inv(np.eye(naux) + U)
     Urt = scipy.linalg.sqrtm(U)
+    if Urt.dtype == complex:
+        if abs(Urt.imag).max() > 1e-8:
+            raise ImaginaryPartError("Complex square root occured in RIRPA calculation.")
+        else:
+            Urt = Urt.real
     # Evaluate the resulting RI
     if type(ri) == np.ndarray:
         return einsum("p,np,nm->mp", D ** (-1), ri_L, Urt)

vayesta/rpa/rirpa/momzero_NI.py
@@ -162,7 +162,7 @@ class BaseMomzeroOffset(NumericalIntegratorBase):
 
     def eval_contrib(self, freq):
         # This should be real currently, so can safely do this.
-        expval = np.exp(-freq * self.D).real
+        expval = np.exp(-freq * self.D)
         lrot = einsum("lp,p->lp", self.target_rot, expval)
         rrot = expval
         res = dot(dot(lrot, self.S_L.T), einsum("np,p->np", self.S_R, rrot))

vayesta/solver/ebfci.py
@@ -85,10 +85,10 @@ class EBFCI_Solver(FCI_Solver):
             results.dm1, results.dm2 = ebfci_slow.make_rdm12e(civec, self.nactive, nelec)
             # Calculating only the components of the dd response moments we needs cuts down on calculation time.
             frag_coeffs = np.linalg.multi_dot([self.fragment.c_active.T, self.base.get_ovlp(), self.fragment.c_frag])
-            dd_moms = ebfci_slow.calc_dd_response_moment_spatial(civec, e_fci, 1, self.nactive, nelec,
-                                                        self.nbos, heff, eris, np.diag(self.bos_freqs), self.eb_coupling,
-                                                        self.opts.bos_occ_cutoff, results.dm1, trace = False,
-                                                        coeffs = frag_coeffs)
+            dd_moms = ebfci_slow.calc_dd_resp_mom(civec, e_fci, 1, self.nactive, nelec,
+                                                  self.nbos, heff, eris, np.diag(self.bos_freqs), self.eb_coupling,
+                                                  self.opts.bos_occ_cutoff, results.dm1, trace = False,
+                                                  coeffs = frag_coeffs)
             results.dd_mom0 = dd_moms[0]
             results.dd_mom1 = dd_moms[1]
         elif self.opts.make_rdm2:

vayesta/solver/ebfci2.py
@@ -90,7 +90,7 @@ class EBFCI_Solver(FCI_Solver):
         except AttributeError:
             dm1 = self.make_rdm1(civec)
 
-        self.dd_moms = ebfci_slow.calc_dd_response_moment_spatial(
+        self.dd_moms = ebfci_slow.calc_dd_resp_mom(
             civec, self.e_fci, max_mom, self.ncas, self.nelec, self.nbos, heff, eris,
             np.diag(self.fragment.bos_freqs), self.fragment.couplings, self.opts.max_boson_occ, dm1)
         return self.dd_moms