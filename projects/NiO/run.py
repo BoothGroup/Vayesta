15f11faef0bae0212c753a235050ba3d68903759
Max Nusspickel <max.nusspickel@googlemail.com>
2022-02-15 18:23:19 +0000

    Updates project scripts

--- projects/NiO/run.py
+++ projects/NiO/run.py
@@ -1,4 +1,5 @@
 import numpy as np
+import argparse
 
 import pyscf
 import pyscf.pbc
@@ -8,6 +9,14 @@ import pyscf.pbc.tools
 import vayesta
 import vayesta.ewf
 from vayesta.misc import solids
+from vayesta.misc import scf_with_mpi
+from vayesta.core.mpi import mpi
+
+#from vayesta.misc.plotting.plotly_mol import plot_mol
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--kmesh', type=int, nargs=3, default=None)
+args = parser.parse_args()
 
 # Experimental Lattice constant
 a0 = 4.4448
@@ -15,45 +24,90 @@ a0 = 4.4448
 cell = pyscf.pbc.gto.Cell()
 cell.a, cell.atom = solids.rocksalt(atoms=['Ni', 'O'], a=a0)
 cell.basis = 'def2-svp'
-cell.output = 'pyscf.out'
+#cell.basis = 'gth-dzvp'
+#cell.pseudo = 'gth-pade'
+cell.output = 'pyscf.mpi%d.out' % mpi.rank
+cell.verbose = 10
 cell.build()
 
 # Make magnetic supercell [8x Ni, 8x O]
 supercell = [2,2,2]
 cell = pyscf.pbc.tools.super_cell(cell, supercell)
 
-kpts = None
-#kpts = cell.make_kpts([2,2,2])
+#fig = plot_mol(cell)
+#fig.write_html('nio.html')
+
+if args.kmesh is not None:
+    kpts = cell.make_kpts(args.kmesh)
+else:
+    kpts = None
 
 # Hartree-Fock
 if kpts is not None:
     mf = pyscf.pbc.scf.KUHF(cell, kpts)
 else:
     mf = pyscf.pbc.scf.UHF(cell)
-mf = mf.density_fit(auxbasis='def2-svp-ri')
-mf.kernel()
+#mf = mf.density_fit(auxbasis='def2-svp-ri')
+mf = mf.rs_density_fit(auxbasis='def2-svp-ri')
+#mf = mf.rs_density_fit()
+#mf.with_df._cderi_to_save = 'cderi.h5'
+mf = scf_with_mpi(mf)
+
+# Create starting guess for AF2 solution:
+ni3d_a = cell.search_ao_label(['^%d Ni 3d' % i for i in [0, 2, 4, 6]])
+ni3d_b = cell.search_ao_label(['^%d Ni 3d' % i for i in [8, 10, 12, 14]])
+ni3d_a = np.ix_(ni3d_a, ni3d_a)
+ni3d_b = np.ix_(ni3d_b, ni3d_b)
+dm_init = mf.init_guess_by_minao(breaksym=False)
+assert np.allclose(dm_init[0], dm_init[1])
+ddm = 0.2*dm_init[0][ni3d_a]
+dm_init[0][ni3d_a] += ddm
+dm_init[0][ni3d_b] -= ddm
+dm_init[1][ni3d_b] += ddm
+dm_init[1][ni3d_a] -= ddm
+assert not np.allclose(dm_init[0], dm_init[1])
+if kpts is not None:
+    dm_init = np.repeat(dm_init[:,None,:,:], len(kpts), axis=1)
+
+#dm_init = mf.get_init_guess()
+#for i, (shell0, shell1, ao0, ao1) in enumerate(cell.aoslice_by_atom()):
+#    aos = np.s_[ao0:ao1]
+#    ni3d = np.isin(ao_labels[aos], '3d')
+#    # Ni (up)
+#    if i in (0, 2, 4, 6):
+#        ddm = dm_init[0][aos,aos]
+#        dm_init[0][aos,aos] += 0.1
+#        dm_init[1][aos,aos] -= 0.1
+#    # Ni (down)
+#    if i in (8, 10, 12, 14):
+#        dm_init[0][aos,aos] -= 0.1
+#        dm_init[1][aos,aos] += 0.1
+mf.kernel(dm_init)
 
 # Embedded calculation will automatically unfold the k-point sampled mean-field
 eta = 1e-6
-emb = vayesta.ewf.EWF(mf, bno_threshold=eta)
-emb.iao_fragmentation()
-ni1 = emb.add_atomic_fragment(0, sym_factor=4)
-ni2 = emb.add_atomic_fragment(2, sym_factor=4)
-o1 = emb.add_atomic_fragment(1, sym_factor=4)
-o2 = emb.add_atomic_fragment(3, sym_factor=4)
-emb.kernel()
+emb = vayesta.ewf.EWF(mf, bno_threshold=eta, store_dm1=True, dmet_threshold=1e-4)
+#emb.iao_fragmentation()
+#ni1 = emb.add_atomic_fragment(0, sym_factor=4)
+#ni2 = emb.add_atomic_fragment(8, sym_factor=4)
+#o1 = emb.add_atomic_fragment(1, sym_factor=4)
+#o2 = emb.add_atomic_fragment(9, sym_factor=4)
 
-emb.t1_diagnostics()
+#emb.pop_analysis(dm1_init, local_orbitals='iao+pao', filename='pop-init-iao.txt')
 
 dm1_mf = emb.mf.make_rdm1()
 emb.pop_analysis(dm1_mf, local_orbitals='mulliken', filename='pop-mf-mulliken.txt')
 emb.pop_analysis(dm1_mf, local_orbitals='lowdin', filename='pop-mf-lowdin.txt')
 emb.pop_analysis(dm1_mf, local_orbitals='iao+pao', filename='pop-mf-iao.txt')
 
+emb.kernel()
+
 dm1_cc = emb.make_ccsd_rdm1()
 emb.pop_analysis(dm1_cc, local_orbitals='mulliken', filename='pop-cc-mulliken.txt')
 emb.pop_analysis(dm1_cc, local_orbitals='lowdin', filename='pop-cc-lowdin.txt')
 emb.pop_analysis(dm1_cc, local_orbitals='iao+pao', filename='pop-cc-iao.txt')
+
+emb.t1_diagnostic()
 
 print("E(HF)=         %+16.8f Ha" % (mf.e_tot))
 print("E(Emb. CCSD)=  %+16.8f Ha" % (emb.e_tot))
--- projects/no2_graphene/run.py
+++ projects/no2_graphene/run.py
@@ -1,6 +1,7 @@
 import argparse
 import string
 import random
+import os.path
 
 import numpy as np
 
@@ -46,16 +47,22 @@ parser.add_argument('--lindep-threshold', type=float)
 # --- MF
 parser.add_argument('--xc', default=None)
 parser.add_argument('--mf-only', action='store_true')
-parser.add_argument('--scf-max-cycle', type=int, default=50)
-parser.add_argument('--scf-conv-tol', type=float, default=1e-9)
+parser.add_argument('--scf-max-cycle', type=int, default=100)
+parser.add_argument('--scf-conv-tol', type=float, default=1e-8)
 parser.add_argument('--scf-with-mpi', action='store_true')
 # --- Embedding
+parser.add_argument('--solver')
 parser.add_argument('--fragment-type', default='iao')
 parser.add_argument('--nc', type=int, default=0)
 parser.add_argument('--atomic-fragments', action='store_true')
+parser.add_argument('--save-df', action='store_true')
+parser.add_argument('--load-df')
+parser.add_argument('--load-df-miniter', type=int)
+parser.add_argument('--load-scf')
 parser.add_argument('--eta', type=float, default=1e-7)
 parser.add_argument('--eta-no2', type=float, default=1e-9)
 parser.add_argument('--dmet-threshold', type=float, default=1e-4)
+parser.add_argument('--smearing', type=float)
 args =parser.parse_args()
 
 if args.z_graphene is None:
@@ -97,6 +104,7 @@ else:
 if args.invert_scan:
     gates = gates[::-1]
 
+mfconv = False
 dm1_mf = None
 for idx, gate in enumerate(gates):
 
@@ -142,16 +150,29 @@ for idx, gate in enumerate(gates):
     if args.auxbasis is not None:
         auxbasis = args.auxbasis
     else:
-        if isinstance(args.basis, dict):
-            auxbasis = {key : ('%s-ri' % args.basis[key]) for key in args.basis}
+        if isinstance(cell.basis, dict):
+            auxbasis = {key : ('%s-ri' % cell.basis[key]) for key in cell.basis}
         else:
             auxbasis = '%s-ri' % args.basis
     print("RI basis: %s" % auxbasis)
     mf = mf.density_fit(auxbasis=auxbasis)
-    mf.with_df._j_only = False
+    df = mf.with_df
+    df._j_only = False
+    #cderi = ('cderi-%.2f' % gate)
+    cderi = 'cderi.h5'
+    #cderi = 'cderi-%s' % ''.join(random.choice(string.ascii_lowercase) for i in range(16))
+    if args.load_df is not None:
+        if args.load_df_miniter is None or idx > 0:
+            df._cderi = args.load_df
+            print("Loading CDERI: '%s'" % df._cderi)
+            # Build auxcell
+            df.build(with_j3c=False)
     # Keep file for other MPI ranks:
-    cderi = 'cderi-%s' % ''.join(random.choice(string.ascii_lowercase) for i in range(16))
-    mf._cderi_to_save = cderi
+    if args.save_df and df._cderi is None:
+        print("Saving CDERI: '%s'" % cderi)
+        df._cderi_to_save = cderi
+    # Checkpoint file
+    mf.chkfile = ('mf-%.2f.chk' % gate)
 
     # Gate potential
     if gate:
@@ -166,19 +187,39 @@ for idx, gate in enumerate(gates):
         v_gate = None
         hcore_orig = hcore_gate = mf.get_hcore
 
+    # Smearing
+    if args.smearing is not None:
+        mf = pyscf.pbc.scf.addons.smearing_(mf, sigma=args.smearing, method='fermi')
+
     if args.scf_with_mpi:
         mf = scf_with_mpi(mf)
 
     # Run MF
-    if args.trace:
+    if (args.trace and mfconv):
         print("Running HF with initial guess")
-        mf.kernel(dm0=dm1_mf)
+        assert (dm1_mf is not None)
+    elif args.load_scf:
+        load_scf = args.load_scf
+        if '%' in load_scf:
+            load_scf = (args.load_scf % gate)
+        print("Loading initial guess for HF from file '%s'" % load_scf)
+        dm1_mf = mf.from_chk(load_scf)
     else:
         print("Running HF without initial guess")
-        mf.kernel()
+        dm1_mf = mf.get_init_guess()
+
+    mf.kernel(dm0=dm1_mf)
     dm1_mf = mf.make_rdm1()
 
-    if not mf.converged:
+    # Occupations
+    if args.smearing is not None:
+        print("Alpha occupation:")
+        print(mf.mo_occ[0])
+        print("Beta occupation:")
+        print(mf.mo_occ[1])
+
+    mfconv = mf.converged
+    if not mfconv:
         print("MF not converged at gate= %.3f !" % gate)
 
     # MF energy without gate
@@ -197,7 +238,11 @@ for idx, gate in enumerate(gates):
         mf_hf.__dict__.update(mf.__dict__)
         mf = mf_hf
 
-    opts = dict(make_rdm1=True, dmet_threshold=args.dmet_threshold)
+    opts = dict(dmet_threshold=args.dmet_threshold)
+    if args.solver is not None:
+        opts['solver'] = args.solver
+    else:
+        opts['make_rdm1'] = True
     #if v_gate is not None:
     #    # At this point we need to fold the hcore_orig to the supercell
     #    if kpts is not None:
@@ -213,9 +258,10 @@ for idx, gate in enumerate(gates):
     else:
         emb = vayesta.ewf.EWF(mf, bno_threshold=args.eta, **opts)
 
-    emb.pop_analysis(dm1=emb.mf.make_rdm1(), local_orbitals='mulliken', filename='mulliken-mf-%.3f.pop' % gate)
-    emb.pop_analysis(dm1=emb.mf.make_rdm1(), local_orbitals='lowdin',   filename='lowdin-mf-%.3f.pop' % gate)
-    emb.pop_analysis(dm1=emb.mf.make_rdm1(), local_orbitals='iao+pao',  filename='iao+pao-mf-%.3f.pop' % gate)
+    dm_mf = emb.mf.make_rdm1()
+    emb.pop_analysis(dm1=dm_mf, local_orbitals='mulliken', filename='mulliken-mf-%.3f.pop' % gate)
+    emb.pop_analysis(dm1=dm_mf, local_orbitals='lowdin',   filename='lowdin-mf-%.3f.pop' % gate)
+    emb.pop_analysis(dm1=dm_mf, local_orbitals='iao+pao',  filename='iao+pao-mf-%.3f.pop' % gate)
 
     if not args.mf_only:
 
@@ -237,7 +283,10 @@ for idx, gate in enumerate(gates):
             emb.kernel()
 
             # NEW CCSD DM:
-            dm1 = emb.make_rdm1_ccsd()
+            if args.solver in (None, 'CCSD'):
+                dm1 = emb.make_rdm1_ccsd()
+            elif args.solver == 'MP2':
+                dm1 = emb.make_rdm1_mp2()
 
             # Check DM
             if mpi.is_master:
@@ -273,7 +322,8 @@ for idx, gate in enumerate(gates):
             frag.pop_analysis(local_orbitals='lowdin',   filename='lowdin-cc-%.3f.pop' % gate)
             frag.pop_analysis(local_orbitals='iao+pao',  filename='iao+pao-cc-%.3f.pop' % gate)
 
-        emb.t1_diagnostic()
+        if args.solver in (None, 'CCSD'):
+            emb.t1_diagnostic()
 
         # Energy including gate
         e_hf_gate = emb.e_mf
@@ -290,17 +340,19 @@ for idx, gate in enumerate(gates):
             else:
                 hcore_for_energy = lambda emb, *args : hcore_orig(*args)
             #opts['overwrite'] = dict(get_hcore_for_energy=hcore_for_energy)
-        emb.get_hcore_for_energy = hcore_for_energy.__get__(emb)
+            emb.get_hcore_for_energy = hcore_for_energy.__get__(emb)
 
         e_hf = emb.e_mf
         e_cc = emb.e_tot
 
     else:
+        e_hf = e_hf_gate = np.nan
         e_cc = e_cc_gate = np.nan
 
     if mpi.is_master:
         with open('energies.txt', 'a') as f:
-            f.write('%.4f  % 16.8f  % 16.8f  % 16.8f\n' % (gate, e_mf, e_hf, e_cc))
+            f.write('%.4f  %d  % 16.8f  % 16.8f  % 16.8f\n' % (gate, mfconv, e_mf, e_hf, e_cc))
 
         with open('energies-gate.txt', 'a') as f:
-            f.write('%.4f  % 16.8f  % 16.8f  % 16.8f\n' % (gate, e_mf_gate, e_hf_gate, e_cc_gate))
+            f.write('%.4f  %d  % 16.8f  % 16.8f  % 16.8f\n' % (gate, mfconv, e_mf_gate, e_hf_gate, e_cc_gate))
+
