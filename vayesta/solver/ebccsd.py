2ae0692bf8401e4d3c3c84c2e0de9e25dd6c2eb1
Charles Scott <cjcargillscott@gmail.com>
2022-07-01 12:36:17 +0100

    Ensure EBCCSD correctly applies chemical potential.

--- vayesta/solver/ebccsd.py
+++ vayesta/solver/ebccsd.py
@@ -39,6 +39,8 @@ class EBCCSD_Solver(EBClusterSolver):
         if eris is None:
             eris = self.get_eris()
         f_act = dot(self.cluster.c_active.T, self.base.get_fock(), self.cluster.c_active)
+        if self.v_ext is not None:
+            f_act += self.v_ext
         couplings = self.fragment.couplings
         if self.opts.polaritonic_shift:
             fock_shift, coupling_shift = self.get_polaritonic_shift(self.fragment.bos_freqs, self.fragment.couplings)
@@ -157,7 +159,9 @@ class UEBCCSD_Solver(EBCCSD_Solver):
             fock_shift, coupling_shift = self.get_polaritonic_shift(self.fragment.bos_freqs, self.fragment.couplings)
             f_act = tuple([x + y for x, y in zip(f_act, fock_shift)])
             couplings = tuple([x + y for x, y in zip(couplings, coupling_shift)])
-
+            if  self.v_ext is not None:
+               f_act = ((f_act[0] + self.v_ext[0]),
+                         (f_act[1] + self.v_ext[1]))
         return (f_act, eris, self.cluster.nocc_active, self.cluster.nvir_active), \
                tuple([x.transpose(0, 2, 1) for x in couplings])
 
