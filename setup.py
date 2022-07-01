edfc1183d918fe82532277bd9f8274fa6bd00991
Ollie Backhouse <olbackhouse@gmail.com>
2022-07-01 10:29:09 +0100

    More work on setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
+#!/usr/bin/env python3
+
 import os
-import functools
 import subprocess
 from setuptools import setup, find_packages, Extension
+from setuptools.command.test import test
 from setuptools.command.build_ext import build_ext
+
+setup_src = os.path.abspath(os.path.join(__file__, ".."))
 
 
 class CMakeBuild(build_ext):
     def build_cmake(self, ext):
-        src = os.path.abspath(os.path.join(__file__, "..", "pyscf", "lib"))
+        src = os.path.join(setup_src, "vayesta", "libs")
 
         cmake_args = [f"-S{src}", f"-B{self.build_temp}"]
         build_args = []
@@ -22,11 +26,36 @@ class CMakeBuild(build_ext):
                 ["cmake", ext.sourcedir, *cmake_args],
                 cwd=self.build_temp,
         )
-
         subprocess.check_call(
                 ["cmake", "--build", ".", *build_args],
                 cwd=self.build_temp,
         )
+
+
+class DiscoverTests(test):
+    user_options = [
+            ("include-veryslow", "i", "Include tests marked as veryslow"),
+    ]
+
+    def initialize_options(self):
+        test.initialize_options(self)
+        self.include_veryslow = False
+
+    def finalize_options(self):
+        pass
+
+    def run_tests(self):
+        # Only import pytest in this scope
+        import pytest
+
+        src = os.path.join(setup_src, "vayesta", "tests")
+
+        test_args = []
+        if not self.include_veryslow:
+            test_args.append("-m not veryslow")
+
+        pytest.main([src, *test_args])
+
 
 
 setup(
@@ -34,56 +63,59 @@ setup(
     version="0.0.0",
     description="A toolkit for quantum embedding methods",
     url="https://vayesta.github.io",
-    project_urls={
-        # TODO tutorials, docs
-        "source": "https://github.com/BoothGroup/Vayesta",
-        "issues": "https://github.com/BoothGroup/Vayesta/issues",
-    },
+    download_url="https://github.com/BoothGroup/Vayesta",
     keywords=[
-        "embedding",
-        "quantum", "chemistry",
-        "material", "science",
-        "electronic", "structure",
-        "DMET", "RPA",
+            "embedding",
+            "quantum", "chemistry",
+            "material", "science",
+            "electronic", "structure",
+            "dmet", "rpa",
     ],
     author=", ".join([
-        "M. Nusspickel",
-        "O. J. Backhouse",
-        "B. Ibrahim",
-        "A. Santana-Bonilla",
-        "C. J. C. Scott",
-        "G. H. Booth",
+            "M. Nusspickel",
+            "O. J. Backhouse",
+            "B. Ibrahim",
+            "A. Santana-Bonilla",
+            "C. J. C. Scott",
+            "G. H. Booth",
     ]),
     author_email="vayesta.embedding@gmail.com",
     license="Apache License 2.0",  # FIXME?
     platforms=[
-        "Linux",
-        "Mac OS-X",
+            "Linux",
+            "Mac OS-X",
     ],
     python_requires=">=3.7",
-    classifier=[
-        "Development Status :: 2 - Pre-Alpha",
-        "License :: OSI Approved :: Apache Software License",  # FIXME?
-        "Intended Audience :: Science/Research",
-        "Intended Audience :: Developers",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Software Development",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: C",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: POSIX :: Linux",
+    classifiers=[
+            "Development Status :: 2 - Pre-Alpha",
+            "License :: OSI Approved :: Apache Software License",  # FIXME?
+            "Intended Audience :: Science/Research",
+            "Intended Audience :: Developers",
+            "Topic :: Scientific/Engineering",
+            "Topic :: Software Development",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: C",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: POSIX :: Linux",
     ],
     package=find_packages(exclude=["*test*", "*examples*"]),
     include_package_data=True,
-    install_requires=[  # FIXME mpi4py, cvxpy
-        "numpy>=1.19.0",
-        "scipy>=1.2",
-        "h5py>=2.7",
-        "pyscf @ git+https://github.com/pyscf/pyscf@master#egg=pyscf",  # FIXME when pyscf wheels update
+    install_requires=[
+            "numpy>=1.19.0",
+            "scipy>=1.2",
+            "h5py>=2.7",
+            "cvxpy>=1.1",
+            "mpi4py>=2.0.0",
+            "pyscf @ git+https://github.com/pyscf/pyscf@master#egg=pyscf",  # FIXME when pyscf wheels update
     ],
+    ext_modules=[Extension("vayesta_lib", [])],
+    cmdclass={
+            "build_ext": CMakeBuild,
+            "test": DiscoverTests,
+    },
     tests_require=[
             "pytest",
             "pytest-cov",
