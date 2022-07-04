cd5888b49fa65d20272aeaa2ef503d6af6bd02a8
Max Nusspickel <max.nusspickel@googlemail.com>
2022-07-04 01:02:22 +0100

    Updates example

--- examples/ewf/molecules/15-chemical-potential.py
+++ examples/ewf/molecules/15-chemical-potential.py
@@ -1,10 +1,7 @@
 import numpy as np
-
 import pyscf
 import pyscf.gto
 import pyscf.scf
-import pyscf.cc
-
 import vayesta
 import vayesta.ewf
 
@@ -15,53 +12,45 @@ H  0.0000   0.7572  -0.4692
 H  0.0000  -0.7572  -0.4692
 """
 mol.basis = 'cc-pVDZ'
-mol.output = 'pyscf-01.out'
 mol.build()
 
 # Hartree-Fock
 mf = pyscf.scf.RHF(mol)
-mf.conv_tol = 1e-12
 mf.kernel()
-assert mf.converged
 
-# CCSD
-cc = pyscf.cc.CCSD(mf)
-cc.kernel()
+eta = 1e-4
+# --- Standard embedded CCSD
+emb = vayesta.ewf.EWF(mf, bath_options=dict(threshold=eta), solver_options=dict(solve_lambda=True))
+with emb.sao_fragmentation() as frag:
+    frag.add_all_atomic_fragments()
+emb.kernel()
+# Population analyis:
+[print(x.results.wf.l1.shape) for x in emb.fragments]
+dm1 = emb.make_rdm1(ao_basis=True)
+emb.pop_analysis(dm1, filename='pop.txt')
 
-bno = 1e-6
-ecc = vayesta.ewf.EWF(mf, bno_threshold=bno, solve_lambda=True)
-ecc.sao_fragmentation()
-ecc.add_all_atomic_fragments()
-ecc.kernel()
+# --- Embedded CCSD with target number of electrons per fragment:
+emb_nelec = vayesta.ewf.EWF(mf, bath_options=dict(threshold=eta), solver_options=dict(solve_lambda=True))
+with emb_nelec.sao_fragmentation() as frag:
+    frag.add_atomic_fragment(0, nelectron_target=8)
+    frag.add_atomic_fragment(1, nelectron_target=1)
+    frag.add_atomic_fragment(2, nelectron_target=1)
+emb_nelec.kernel()
+# Population analyis:
+dm1 = emb_nelec.make_rdm1(ao_basis=True)
+emb_nelec.pop_analysis(dm1, filename='pop-nelec.txt')
 
-dm1 = mf.make_rdm1()
-ecc.pop_analysis(dm1, filename='pop-mf.txt')
+# --- Embedded CCSD with chemical potential, to ensure that democratically partitioned 1-DM has correct trace:
+emb_cpt = vayesta.ewf.EWF(mf, bath_options=dict(threshold=eta), solver_options=dict(solve_lambda=True))
+emb_cpt.optimize_chempot()
+with emb_cpt.sao_fragmentation() as frag:
+    frag.add_all_atomic_fragments()
+emb_cpt.kernel()
+# Population analyis:
+dm1 = emb_cpt.make_rdm1(ao_basis=True)
+emb_cpt.pop_analysis(dm1, filename='pop-cpt.txt')
 
-dm1 = cc.make_rdm1(ao_repr=True)
-ecc.pop_analysis(dm1, filename='pop-cc.txt')
-
-dm1 = ecc.make_rdm1_demo(ao_basis=True)
-ecc.pop_analysis(dm1, filename='pop-pdm.txt')
-
-dm1 = ecc.make_rdm1_ccsd(ao_basis=True)
-ecc.pop_analysis(dm1, filename='pop-pwf.txt')
-
-
-print("E%-11s %+16.8f Ha" % ('(HF)=', mf.e_tot))
-print("E%-11s %+16.8f Ha" % ('(EWF-CCSD)=', ecc.e_tot))
-
-ecc2 = vayesta.ewf.EWF(mf, bno_threshold=bno, solve_lambda=True)
-ecc2.sao_fragmentation()
-ecc2.add_atomic_fragment(0, nelectron_target=8)
-ecc2.add_atomic_fragment(1, nelectron_target=1)
-ecc2.add_atomic_fragment(2, nelectron_target=1)
-ecc2.kernel()
-
-dm1 = ecc2.make_rdm1_demo(ao_basis=True)
-ecc2.pop_analysis(dm1, filename='pop-pdm-cpt.txt')
-
-dm1 = ecc2.make_rdm1_ccsd(ao_basis=True)
-ecc2.pop_analysis(dm1, filename='pop-pwf-cpt.txt')
-
-print("E%-11s %+16.8f Ha" % ('(HF)=', mf.e_tot))
-print("E%-11s %+16.8f Ha" % ('(EWF-CCSD)=', ecc2.e_tot))
+print("E(HF)=                             %+16.8f Ha" % mf.e_tot)
+print("E(emb. CCSD)=                      %+16.8f Ha" % emb.e_tot)
+print("E(emb. CCSD + electron target)=    %+16.8f Ha" % emb_nelec.e_tot)
+print("E(emb. CCSD + chemical potential)= %+16.8f Ha" % emb_cpt.e_tot)
