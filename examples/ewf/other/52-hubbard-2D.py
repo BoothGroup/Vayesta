c633670c0fe56096af72ea61d1ccd666dff070c7
Max Nusspickel <max.nusspickel@googlemail.com>
2022-07-04 01:26:41 +0100

    Update EWF Hubbard examples

--- examples/ewf/other/52-hubbard-2D.py
+++ examples/ewf/other/52-hubbard-2D.py
@@ -2,6 +2,7 @@ import vayesta
 import vayesta.ewf
 import vayesta.lattmod
 
+
 nsites = (4,4)
 fragment = (2, 2)
 hubbard_u = 6.0
@@ -11,14 +12,15 @@ nsite = nsites[0]*nsites[1]
 nfrag = fragment[0]*fragment[1]
 nelectron = nsite
 mol = vayesta.lattmod.Hubbard2D(nsites, nelectron=nelectron, hubbard_u=hubbard_u, boundary=boundary, tiles=fragment)
-mol.output = "pyscf-52.out"
 mf = vayesta.lattmod.LatticeMF(mol)
 mf.kernel()
 
-ewf = vayesta.ewf.EWF(mf, solver='FCI', fragment_type='Site', bath_type=None, make_rdm1=True, make_rdm2=True)
-for i in range(0, nsite, nfrag):
-    ewf.add_atomic_fragment(list(range(i, i + nfrag)), nelectron_target=nfrag)
-ewf.kernel()
+emb = vayesta.ewf.EWF(mf, solver='FCI', bath_options=dict(bathtype='dmet'))
+with emb.site_fragmentation() as frag:
+    for i in range(0, nsite, nfrag):
+        frag.add_atomic_fragment(list(range(i, i + nfrag)), nelectron_target=nfrag)
+emb.kernel()
+
 print("E(HF)=       %+16.8f Ha" % (mf.e_tot/nelectron))
-print("E(EWF-FCI)=  %+16.8f Ha" % (ewf.e_tot/nelectron))
-print("E(DMET)=     %+16.8f Ha" % (ewf.get_dmet_energy()/nelectron))
+print("E(Emb. FCI)= %+16.8f Ha" % (emb.e_tot/nelectron))
+print("E(DMET)=     %+16.8f Ha" % (emb.get_dmet_energy()/nelectron))
