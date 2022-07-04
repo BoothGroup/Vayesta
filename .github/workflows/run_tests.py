d541a37fbfb344ac5cb4df64e1caf7dca62c6eb8
Oliver Backhouse <olbackhouse@gmail.com>
2022-07-04 12:00:29 +0100

    Try fixing --cov again

--- .github/workflows/run_tests.py
+++ .github/workflows/run_tests.py
@@ -7,7 +7,7 @@ src = os.path.abspath(os.path.join(__file__, "..", "..", "..", "vayesta"))
 if __name__ == "__main__":
     args = [
         "vayesta/tests",
-        "--cov {src}".format(src=src),
+        "--cov=vayesta",
         "--cov-report xml",
         "--cov-report term",
         "--cov-config .coveragerc",
