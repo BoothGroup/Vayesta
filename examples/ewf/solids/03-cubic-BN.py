0e159dc642052d7681f0e67aad302678ee9fdfac
Max Nusspickel <max.nusspickel@googlemail.com>
2022-07-07 15:07:24 +0100

    Change diamond to cBN example

--- examples/ewf/solids/03-cubic-BN.py
+++ examples/ewf/solids/03-cubic-BN.py
@@ -8,19 +8,13 @@ import vayesta.ewf
 
 
 cell = pyscf.pbc.gto.Cell()
-#a = 3.57
-#a = 5.41
 a = 3.615
-#cell.atom = 'C 0 0 0 ; C %f %f %f' % (a/4, a/4, a/4)
 cell.atom = 'B 0 0 0 ; N %f %f %f' % (a/4, a/4, a/4)
-#cell.atom = 'Zn 0 0 0 ; S %f %f %f' % (a/4, a/4, a/4)
 cell.a = np.asarray([
     [a/2, a/2, 0],
     [0, a/2, a/2],
     [a/2, 0, a/2]])
 cell.basis = 'sto-6g'
-#cell.basis = 'gth-szv'
-#cell.pseudo = 'gth-pade'
 cell.output = 'pyscf.out'
 cell.build()
 
@@ -29,7 +23,6 @@ kmesh = [2,2,2]
 kpts = cell.make_kpts(kmesh)
 mf = pyscf.pbc.scf.KRHF(cell, kpts)
 mf = mf.density_fit(auxbasis='sto-6g')
-#mf = mf.density_fit()
 mf.kernel()
 
 # Full system CCSD
@@ -37,6 +30,7 @@ cc = pyscf.pbc.cc.KCCSD(mf)
 cc.kernel()
 
 # Embedded calculation will automatically fold the k-point sampled mean-field to the supercell
+# solve_lambda=True is required, if density-matrix is needed!
 emb = vayesta.ewf.EWF(mf, bath_options=dict(threshold=1e-6), solver_options=dict(solve_lambda=True))
 emb.kernel()
 
@@ -44,8 +38,9 @@ print("E(HF)=             %+16.8f Ha" % mf.e_tot)
 print("E(Emb. CCSD)=      %+16.8f Ha" % emb.e_tot)
 print("E(CCSD)=           %+16.8f Ha" % cc.e_tot)
 
-# Population analysis (q: charge, s: spin)
+# One-body density matrix in the supercell AO-basis
 dm1 = emb.make_rdm1(ao_basis=True)
+# Population analysis (q: charge, s: spin)
 # Possible options for local_orbitals: 'mulliken', 'lowdin', 'iao+pao', or custom N(AO) x N(AO) coefficient matrix
 # orbital_resolved=True is used to print orbital resolved (rather than only atom resolved) analysis
 emb.pop_analysis(dm1, local_orbitals='iao+pao', orbital_resolved=True)
