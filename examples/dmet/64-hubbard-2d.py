22005cb605d5a83d326db793fef372784bda60a1
Max Nusspickel <max.nusspickel@googlemail.com>
2022-07-07 11:57:37 +0100

    Updates DMET Hubbard examples

--- examples/dmet/64-hubbard-2d.py
+++ examples/dmet/64-hubbard-2d.py
@@ -8,31 +8,37 @@ import vayesta.lattmod
 
 
 nsites = [6,6]
-impshape = [2,2]
+nimps = [2,2]
 hubbard_u = 6.0
 # Anti-PBC and mixed boundaries do not work with translational symmetry!
 boundary = 'PBC'
 nsite = nsites[0]*nsites[1]
 nelectron = nsite-10
-nimp = impshape[0]*impshape[1]
-mol = vayesta.lattmod.Hubbard2D(nsites, nelectron=nelectron, hubbard_u=hubbard_u, boundary=boundary, tiles=impshape, output='pyscf.out')
+nimp = nimps[0]*nimps[1]
+mol = vayesta.lattmod.Hubbard2D(nsites, nelectron=nelectron, hubbard_u=hubbard_u, boundary=boundary, tiles=nimps)
 mf = vayesta.lattmod.LatticeMF(mol)
 mf.kernel()
 
-# Calculate each fragment:
+# Calculate each (2 x 2)-sites fragment:
+# (Note that the Hubbard 2D class will reorder the sites, such that all 2 x 2 tiles contain continuous
+# site indices)
 dmet = vayesta.dmet.DMET(mf, solver='FCI', maxiter=1)
 with dmet.site_fragmentation() as f:
     for site in range(0, nsite, nimp):
-        f.add_atomic_fragment(list(range(site, site + nimp)))
+        f.add_atomic_fragment(list(range(site, site+nimp)))
 dmet.kernel()
 
 # Calculate a single fragment and use translational symmetry:
 dmet_sym = vayesta.dmet.DMET(mf, solver='FCI', maxiter=1)
-dmet_sym.symmetry.set_translations([nsites[0]//impshape[0], nsites[1]//impshape[1], 1])
+# Specify the number of translational copies in direction of the three lattice vectors by passing a list with three
+# integers, [n0, n1, n2]. 1D or 2D systems have their periodic dimension along the first one or two axes.
+nimages = [nsites[0]//nimps[0], nsites[1]//nimps[1], 1]
+dmet_sym.symmetry.set_translations(nimages)
+# Add only a single (2 x 2)-sites fragment:
 with dmet_sym.site_fragmentation() as f:
     frag = f.add_atomic_fragment(list(range(nimp)))
 dmet_sym.kernel()
 
-# Compare converged correlation potential
-print("Difference in converged correlation potentials with and without translational symmetry:")
-print("|d(V_corr)|= %.5e" % np.linalg.norm(dmet.vcorr - dmet_sym.vcorr))
+print("Difference in converged solutions:")
+print("  |d(E_tot)|=  %.5e" % abs(dmet.e_tot - dmet_sym.e_tot))
+print("  |d(V_corr)|= %.5e" % np.linalg.norm(dmet.vcorr - dmet_sym.vcorr))
