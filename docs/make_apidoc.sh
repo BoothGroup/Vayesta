abff1d2d296cffdf97af66093cf32d0349b27a3f
Max Nusspickel <max.nusspickel@googlemail.com>
2022-07-22 10:29:25 +0100

    Exclude ewf/icmp2 from apidocs

--- docs/make_apidoc.sh
+++ docs/make_apidoc.sh
@@ -4,6 +4,7 @@ sphinx-apidoc -o source/apidoc -t templates ../vayesta/ \
 ../vayesta/core/qemb/expval.py \
 ../vayesta/core/vpyscf/** \
 ../vayesta/ewf/amplitudes.py \
+../vayesta/ewf/icmp2.py \
 ../vayesta/ewf/rdm.py \
 ../vayesta/ewf/urdm.py \
 ../vayesta/misc/gmtkn55** \
