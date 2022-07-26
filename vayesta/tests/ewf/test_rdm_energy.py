1f7665497c087ae7fd8e5df7650cf7b87529bbba
Basil Ibrahim <basil.ibrahim@kcl.ac.uk>
2022-07-26 16:27:16 +0100

    UCCSD DM energy test

--- vayesta/tests/ewf/test_rdm_energy.py
+++ vayesta/tests/ewf/test_rdm_energy.py
@@ -33,6 +33,30 @@ class TestRDMEnergyConvergence(TestCase):
         self.assertAlmostEqual(lg, cc.e_corr)
         self.assertAlmostEqual(gg, cc.e_corr)
 
+class TestRDMEnergyConvergenceUHF(TestCase):
+
+    def test_water(self):
+
+        #RHF
+        mf = testsystems.lih_ccpvdz.uhf()
+
+        #CCSD
+        cc = pyscf.cc.UCCSD(mf)
+        cc.kernel()
+
+        #Full bath EWF
+        ewf = vayesta.ewf.EWF(mf, bath_type='full', solve_lambda=True)
+        ewf.kernel()
+
+        ll = ewf._get_dm_corr_energy_old(global_dm1=False, global_dm2=False)
+        gl = ewf._get_dm_corr_energy_old(global_dm1=True, global_dm2=False)
+        lg = ewf._get_dm_corr_energy_old(global_dm1=False, global_dm2=True)
+        gg = ewf._get_dm_corr_energy_old(global_dm1=True, global_dm2=True)
+
+        self.assertAlmostEqual(ll, cc.e_corr)
+        self.assertAlmostEqual(gl, cc.e_corr)
+        self.assertAlmostEqual(lg, cc.e_corr)
+        self.assertAlmostEqual(gg, cc.e_corr)
 
 #    def test_h2_solid(self):
 #
