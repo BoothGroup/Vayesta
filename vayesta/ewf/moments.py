3907c8ab4c609b09dd1c6e39c878622fa41b9fcd
Basil Ibrahim <basil.ibrahim@kcl.ac.uk>
2022-08-20 22:53:54 +0100

    Small changes

--- vayesta/ewf/moments.py
+++ vayesta/ewf/moments.py
@@ -121,7 +121,7 @@ def make_ccsdgf_moms(emb, ao_basis=False, t_as_lambda=False, ovlp_tol=None, svd_
             hea, heiab = fy.ea_moms[2], fy.ea_moms[3]
             cfcy = dot(cfy, cfy.T)
             #hea = np.einsum('aA,npA->npa', cfcy, hea)
-            heiab = einsum('iI,npIab->npija', cfcy, heija)
+            heiab = einsum('iI,npIab->npiab', cfcy, heija)
 
             ea_mom_xy = einsum('pa,aA,nQA->npQ', ba, rxy_vir, hea)
             ea_mom_xy += einsum('piab,iI,aA,bB,nQIAB->npQ', biab, rxy_occ, rxy_vir, rxy_vir, heiab)
@@ -137,3 +137,137 @@ def make_ccsdgf_moms(emb, ao_basis=False, t_as_lambda=False, ovlp_tol=None, svd_
         ea_mom += einsum('pP,nPq', cx, ea_mom_x)
 
     return ip_mom
+
+
+def make_ip_moms(cc, t1, t2, l1, l2, nmom=3, contract_be=True):
+        nocc, nvir = t1.shape
+        nmo = nocc + nvir
+
+        eom = pyscf.cc.eom_rccsd.EOMIP(cc)
+        matvec, diag = eom.gen_matvec()
+
+        bi = np.zeros((nmo, nocc))
+        bija = np.zeros((nmo, nocc, nocc, nvir))
+
+        ei = np.zeros((nmo, nocc))
+        eija = np.zeros((nmo, nocc, nocc, nvir))
+
+        for p in range(nmo):
+            # Build bra vector for orbital p
+            b = pyscf.cc.gfccsd.build_bra_hole(cc, eom, t1, t2, l1, l2, p)
+            # Unpack bra vector into 1h and 2h1p amplitudes
+            b1, b2 = eom.vector_to_amplitudes(b)
+            bi[p] = b1
+            bija[p] = b2
+
+            # Build ket vector for orbital p
+            e = pyscf.cc.gfccsd.build_ket_hole(cc, eom, t1, t2, p)
+            # Unpack ket vector into 1h and 2h1p amplitudes
+            e1, e2 = eom.vector_to_amplitudes(e)
+            ei[p] = e1
+            eija[p] = e2
+
+            t = np.zeros((nmom, nmo, nmo))
+
+
+        if contract_be:
+            # Loop over q orbitals
+            for q in range(nmo):
+                # Loop over moments
+                for n in range(nmom):
+                    # Loop over p orbitals
+                    for p in range(nmo):
+                        # Contract 1h and 2h1p parts of b and e to moment
+                        t[n, p, q] += np.einsum("i,i->", bi[p], ei[q])
+                        t[n, p, q] += np.einsum("ija,ija->", bija[p], eija[q])
+                    if (n+1) != nmom:
+                        # Scale e = H e for the next moment
+                        e_vec = eom.amplitudes_to_vector(ei[q], eija[q])
+                        e_vec = -matvec([e_vec])[0]
+                        ei[q], eija[q] = eom.vector_to_amplitudes(e_vec)
+
+
+            return t
+
+        else:
+            hei = np.zeros((nmom, nmo, nocc))
+            heija = np.zeros((nmom, nmo, nocc, nocc, nvir))
+            for n in range(nmom):
+                # Loop over p orbitals
+                for q in range(nmo):
+                    hei[n,q] += ei[q]
+                    heija[n,q] += eija[q]
+                    if (n+1) != nmom:
+                        # Scale e = H e for the next moment
+                        e_vec = eom.amplitudes_to_vector(ei[q], eija[q])
+                        e_vec = -matvec([e_vec])[0]
+                        ei[q], eija[q] = eom.vector_to_amplitudes(e_vec)
+
+            return bi, bija, hei, heija
+
+def make_ea_moms(cc, t1, t2, l1, l2, nmom=3, contract_be=True):
+        nocc, nvir = t1.shape
+        nmo = nocc + nvir
+
+        eom = pyscf.cc.eom_rccsd.EOMEA(cc)
+        matvec, diag = eom.gen_matvec()
+
+        ba = np.zeros((nmo, nvir))
+        biab = np.zeros((nmo, nocc, nvir, nvir))
+
+        ea = np.zeros((nmo, nvir))
+        eiab = np.zeros((nmo, nocc, nvir, nvir))
+
+        for p in range(nmo):
+            # Build bra vector for orbital p
+            b = pyscf.cc.gfccsd.build_bra_part(cc, eom, t1, t2, l1, l2, p)
+            # Unpack bra vector into 1h and 2h1p amplitudes
+            b1, b2 = eom.vector_to_amplitudes(b)
+
+            ba[p] = b1
+            biab[p] = b2
+
+            # Build ket vector for orbital p
+            e = pyscf.cc.gfccsd.build_ket_part(cc, eom, t1, t2, p)
+            # Unpack ket vector into 1h and 2h1p amplitudes
+            e1, e2 = eom.vector_to_amplitudes(e)
+            ea[p] = e1
+            eiab[p] = e2
+
+            t = np.zeros((nmom, nmo, nmo))
+
+        if contract_be:
+            # Loop over q orbitals
+            for q in range(nmo):
+                # Loop over moments
+                for n in range(nmom):
+                    # Loop over p orbitals
+                    for p in range(nmo):
+                        # Contract 1h and 2h1p parts of b and e to moment
+                        t[n, p, q] -= np.einsum("i,i->", ba[p], ea[q])
+                        t[n, p, q] -= np.einsum("ija,ija->", biab[p], eiab[q])
+                    if (n+1) != nmom:
+                        # Scale e = H e for the next moment
+                        e_vec = eom.amplitudes_to_vector(ea[q], eiab[q])
+                        e_vec = matvec([e_vec])[0]
+                        ea[q], eiab[q] = eom.vector_to_amplitudes(e_vec)
+
+            return t
+        else:
+            hea = np.zeros((nmom, nmo, nvir))
+            heiab = np.zeros((nmom, nmo, nocc, nvir, nvir))
+            for n in range(nmom):
+                # Loop over p orbitals
+                for q in range(nmo):
+                    hea[n,q] += ea[q]
+                    heiab[n,q] += eiab[q]
+                    if (n+1) != nmom:
+                        # Scale e = H e for the next moment
+                        e_vec = eom.amplitudes_to_vector(ea[q], eiab[q])
+                        e_vec = matvec([e_vec])[0]
+                        ea[q], eiab[q] = eom.vector_to_amplitudes(e_vec)
+
+            return ba, biab, hea, heiab
+
+
+
