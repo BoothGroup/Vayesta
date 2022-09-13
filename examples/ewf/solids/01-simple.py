08d967e61c34e9484bdb4361cdba537623890e3e
Max Nusspickel <max.nusspickel@googlemail.com>
2022-09-13 16:16:46 +0100

    Adds support for KHF with k-point symmetry

--- examples/ewf/solids/01-simple.py
+++ examples/ewf/solids/01-simple.py
@@ -1,21 +1,21 @@
 import numpy as np
-
 import pyscf
 import pyscf.pbc
 import pyscf.pbc.scf
-
 import vayesta
 import vayesta.ewf
+
 
 cell = pyscf.pbc.gto.Cell()
 cell.atom = ['He 0.0 0.0 0.0']
 cell.a = 1.4 * np.eye(3)
 cell.basis = 'def2-svp'
 cell.output = 'pyscf.out'
-
 cell.build()
 
 kpts = cell.make_kpts([2,2,2])
+# Alternatively, use k-point symmetry with:
+# kpts = cell.make_kpts([2,2,2], space_group_symmetry=True, time_reversal_symmetry=True, symmorphic=True)
 
 # Hartree-Fock with k-points
 mf = pyscf.pbc.scf.KRHF(cell, kpts)
