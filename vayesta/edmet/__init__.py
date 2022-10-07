6fb1655f82fbca704e67c924a67e0b5a4963c8c8
Basil Ibrahim <basil.ibrahim@kcl.ac.uk>
2022-10-07 14:05:09 +0100

    Automatically convert ROHF to UHF

--- vayesta/edmet/__init__.py
+++ vayesta/edmet/__init__.py
@@ -5,12 +5,18 @@ email:  cjcargillscott@gmail.com
 
 import pyscf
 import pyscf.scf
+import logging
 
 from .edmet import REDMET
 from .uedmet import UEDMET
+
+log = logging.getLogger(__name__)
 
 def EDMET(mf, *args, **kwargs):
     """Determine restricted or unrestricted by inspection of mean-field object"""
     if isinstance(mf, pyscf.scf.uhf.UHF):
         return UEDMET(mf, *args, **kwargs)
+    elif isinstance(mf, pyscf.scf.rohf.ROHF):
+        log.warning("Converting ROHF reference to UHF")
+        return UEMET(mf.to_uhf(), *args, **kwargs)
     return REDMET(mf, *args, **kwargs)
