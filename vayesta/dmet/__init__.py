6a7e64fe561aed000d849cf197bb6cc93d640bdb
Basil Ibrahim <basil.ibrahim@kcl.ac.uk>
2022-10-07 14:05:09 +0100

    Automatically convert ROHF to UHF

--- vayesta/dmet/__init__.py
+++ vayesta/dmet/__init__.py
@@ -5,6 +5,7 @@ email:  cjcargillscott@gmail.com
 
 import pyscf
 import pyscf.scf
+import logging
 
 try:
     import cvxpy
@@ -14,8 +15,13 @@ except ModuleNotFoundError as e:
 from .dmet import RDMET
 from .udmet import UDMET
 
+log = logging.getLogger(__name__)
+
 def DMET(mf, *args, **kwargs):
     """Determine restricted or unrestricted by inspection of mean-field object"""
     if isinstance(mf, pyscf.scf.uhf.UHF):
         return UDMET(mf, *args, **kwargs)
+    elif isinstance(mf, pyscf.scf.rohf.ROHF):
+        log.warning("Converting ROHF reference to UHF")
+        return UDMET(mf.to_uhf(), *args, **kwargs)
     return RDMET(mf, *args, **kwargs)
