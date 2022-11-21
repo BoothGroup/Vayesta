75103a7febaecf6d3940ea082e00d0bef996873b
George Booth <george.booth@kcl.ac.uk>
2022-11-21 15:54:25 +0000

    Update Hubbard EC-CC example to print out whether converged.

--- examples/ewf/other/61-ext-corr-hubbard-1D.py
+++ examples/ewf/other/61-ext-corr-hubbard-1D.py
@@ -6,8 +6,8 @@ import vayesta.ewf
 import vayesta.lattmod
 
 nsite = 10
-nelectron = nsite + 4
-hubbard_u = 4.0
+nelectron = nsite
+hubbard_u = 5.0
 mol = vayesta.lattmod.Hubbard1D(nsite, nelectron=nelectron, hubbard_u=hubbard_u)
 mf = vayesta.lattmod.LatticeMF(mol)
 mf.kernel()
@@ -40,17 +40,17 @@ with emb.site_fragmentation() as f:
     fci_frags.append(f.add_atomic_fragment([6, 7], solver='FCI', bath_options=dict(bathtype='dmet'), store_wf_type='CCSDTQ', nelectron_target=2*nelectron/nsite))
     fci_frags.append(f.add_atomic_fragment([8, 9], solver='FCI', bath_options=dict(bathtype='dmet'), store_wf_type='CCSDTQ', nelectron_target=2*nelectron/nsite))
     # Add single 'complete' CCSD fragment covering all sites (currently inactive)
-    ccsd_frag = f.add_full_system(solver='CCSD', bath_options=dict(bathtype='full'), solver_options=dict(solve_lambda=False), active=False)
+    ccsd_frag = f.add_full_system(solver='CCSD', bath_options=dict(bathtype='full'), solver_options=dict(solve_lambda=False, init_guess='CISD'), active=False)
 # FCI fragment symmetry not available for external correction
 #fci_frags = fci_frags.add_tsymmetric_fragments(tvecs=[5, 1, 1])
 emb.kernel()
+assert(emb.converged)
 
 def ext_corr(emb, fci_frags, ccsd_frag, mode='external-ccsdv', projectors=0):
     """Setup the external correction from the CCSD fragments.
     Two main options: correction_type='external-ccsdv' and 'external-fciv'. For the important T3 * V contribution to the
     T2 amplitudes, this determines whether the V is expressed in the FCI or CCSD cluster space. The CCSD cluster is
     larger, and hence this is likely to be better, as the correction is longer-ranged (though slightly more expensive).
-    NOTE however that the Hubbard model only has local interactions, so these should be identical.
     The other option is 'projectors', which should be an integer between 0 and 2 (inclusive).
     The larger the number, the more fragment projectors are applied to the correcting T2 contributions, and less
     'bath' correlation from the FCI clusters is used as a constraint in the external correction of the CCSD clusters.
@@ -63,43 +63,60 @@ def ext_corr(emb, fci_frags, ccsd_frag, mode='external-ccsdv', projectors=0):
     ccsd_frag.add_external_corrections(fci_frags, correction_type=mode, projectors=projectors)
     # Run kernel again
     emb.kernel()
-    return emb.e_tot
+    return emb.e_tot, emb.converged
 
-# Change number of projectors and/or Coulomb type, and re-run to get change
 e_extcorr = []
-e_extcorr.append(ext_corr(emb, fci_frags, ccsd_frag, mode='external-ccsdv', projectors=0))
-e_extcorr.append(ext_corr(emb, fci_frags, ccsd_frag, mode='external-ccsdv', projectors=1))
-e_extcorr.append(ext_corr(emb, fci_frags, ccsd_frag, mode='external-ccsdv', projectors=2))
-e_extcorr.append(ext_corr(emb, fci_frags, ccsd_frag, mode='external-fciv', projectors=0))
-e_extcorr.append(ext_corr(emb, fci_frags, ccsd_frag, mode='external-fciv', projectors=1))
-e_extcorr.append(ext_corr(emb, fci_frags, ccsd_frag, mode='external-fciv', projectors=1))
+extcorr_conv = []
+e, conv = ext_corr(emb, fci_frags, ccsd_frag, mode='external-ccsdv', projectors=0)
+e_extcorr.append(e); extcorr_conv.append(conv)
+# Change number of projectors and/or Coulomb type, and re-run to get change
+e, conv = ext_corr(emb, fci_frags, ccsd_frag, mode='external-ccsdv', projectors=0)
+e_extcorr.append(e); extcorr_conv.append(conv)
+e, conv = ext_corr(emb, fci_frags, ccsd_frag, mode='external-ccsdv', projectors=1)
+e_extcorr.append(e); extcorr_conv.append(conv)
+e, conv = ext_corr(emb, fci_frags, ccsd_frag, mode='external-ccsdv', projectors=2)
+e_extcorr.append(e); extcorr_conv.append(conv)
+e, conv = ext_corr(emb, fci_frags, ccsd_frag, mode='external-fciv', projectors=0)
+e_extcorr.append(e); extcorr_conv.append(conv)
+e, conv = ext_corr(emb, fci_frags, ccsd_frag, mode='external-fciv', projectors=1)
+e_extcorr.append(e); extcorr_conv.append(conv)
+e, conv = ext_corr(emb, fci_frags, ccsd_frag, mode='external-fciv', projectors=1)
+e_extcorr.append(e); extcorr_conv.append(conv)
 
 # Compare to a simpler tailoring
 e_tailor = []
-e_tailor.append(ext_corr(emb, fci_frags, ccsd_frag, mode='tailor', projectors=0))
-e_tailor.append(ext_corr(emb, fci_frags, ccsd_frag, mode='tailor', projectors=1))
-e_tailor.append(ext_corr(emb, fci_frags, ccsd_frag, mode='tailor', projectors=2))
+tailor_conv = []
+e, conv = ext_corr(emb, fci_frags, ccsd_frag, mode='tailor', projectors=0)
+e_tailor.append(e) ; tailor_conv.append(conv)
+e, conv = ext_corr(emb, fci_frags, ccsd_frag, mode='tailor', projectors=1)
+e_tailor.append(e) ; tailor_conv.append(conv)
+e, conv = ext_corr(emb, fci_frags, ccsd_frag, mode='tailor', projectors=2)
+e_tailor.append(e) ; tailor_conv.append(conv)
 
 # Compare to a delta-tailoring, where the correction is the difference between full-system
 # CCSD and CCSD in the FCI cluster.
 e_dtailor = []
-e_dtailor.append(ext_corr(emb, fci_frags, ccsd_frag, mode='delta-tailor', projectors=0))
-e_dtailor.append(ext_corr(emb, fci_frags, ccsd_frag, mode='delta-tailor', projectors=1))
-e_dtailor.append(ext_corr(emb, fci_frags, ccsd_frag, mode='delta-tailor', projectors=2))
+dtailor_conv = []
+e, conv = ext_corr(emb, fci_frags, ccsd_frag, mode='delta-tailor', projectors=0)
+e_dtailor.append(e) ; dtailor_conv.append(conv)
+e, conv = ext_corr(emb, fci_frags, ccsd_frag, mode='delta-tailor', projectors=1)
+e_dtailor.append(e) ; dtailor_conv.append(conv)
+e, conv = ext_corr(emb, fci_frags, ccsd_frag, mode='delta-tailor', projectors=2)
+e_dtailor.append(e) ; dtailor_conv.append(conv)
 
-print("E(MF)=                                   %+16.8f Ha" % (mf.e_tot/nsite))
-print("E(CCSD)=                                 %+16.8f Ha" % (cc.e_tot/nsite))
-print("E(FCI)=                                  %+16.8f Ha" % (fci.e_tot/nsite))
-print("E(Emb. FCI, 2-site)=                     %+16.8f Ha" % (emb_simp.e_tot/nsite))
-print("E(EC-CCSD, 2-site FCI, 0 proj, ccsd V)=  %+16.8f Ha" % (e_extcorr[0]/nsite))
-print("E(EC-CCSD, 2-site FCI, 1 proj, ccsd V)=  %+16.8f Ha" % (e_extcorr[1]/nsite))
-print("E(EC-CCSD, 2-site FCI, 2 proj, ccsd V)=  %+16.8f Ha" % (e_extcorr[2]/nsite))
-print("E(EC-CCSD, 2-site FCI, 0 proj, fci V)=   %+16.8f Ha" % (e_extcorr[0]/nsite))
-print("E(EC-CCSD, 2-site FCI, 1 proj, fci V)=   %+16.8f Ha" % (e_extcorr[1]/nsite))
-print("E(EC-CCSD, 2-site FCI, 2 proj, fci V)=   %+16.8f Ha" % (e_extcorr[2]/nsite))
-print("E(T-CCSD, 2-site FCI, 0 proj)=           %+16.8f Ha" % (e_tailor[0]/nsite))
-print("E(T-CCSD, 2-site FCI, 1 proj)=           %+16.8f Ha" % (e_tailor[1]/nsite))
-print("E(T-CCSD, 2-site FCI, 2 proj)=           %+16.8f Ha" % (e_tailor[2]/nsite))
-print("E(DT-CCSD, 2-site FCI, 0 proj)=          %+16.8f Ha" % (e_dtailor[0]/nsite))
-print("E(DT-CCSD, 2-site FCI, 1 proj)=          %+16.8f Ha" % (e_dtailor[1]/nsite))
-print("E(DT-CCSD, 2-site FCI, 2 proj)=          %+16.8f Ha" % (e_dtailor[2]/nsite))
+print("E(MF)=                                   %+16.8f Ha, conv = %s" % (mf.e_tot/nsite, mf.converged))
+print("E(CCSD)=                                 %+16.8f Ha, conv = %s" % (cc.e_tot/nsite, cc.converged))
+print("E(FCI)=                                  %+16.8f Ha, conv = %s" % (fci.e_tot/nsite, fci.converged))
+print("E(Emb. FCI, 2-site)=                     %+16.8f Ha, conv = %s" % (emb_simp.e_tot/nsite, emb_simp.converged))
+print("E(EC-CCSD, 2-site FCI, 0 proj, ccsd V)=  %+16.8f Ha, conv = %s" % ((e_extcorr[0]/nsite), extcorr_conv[0]))
+print("E(EC-CCSD, 2-site FCI, 1 proj, ccsd V)=  %+16.8f Ha, conv = %s" % ((e_extcorr[1]/nsite), extcorr_conv[1]))
+print("E(EC-CCSD, 2-site FCI, 2 proj, ccsd V)=  %+16.8f Ha, conv = %s" % ((e_extcorr[2]/nsite), extcorr_conv[2]))
+print("E(EC-CCSD, 2-site FCI, 0 proj, fci V)=   %+16.8f Ha, conv = %s" % ((e_extcorr[3]/nsite), extcorr_conv[3]))
+print("E(EC-CCSD, 2-site FCI, 1 proj, fci V)=   %+16.8f Ha, conv = %s" % ((e_extcorr[4]/nsite), extcorr_conv[4]))
+print("E(EC-CCSD, 2-site FCI, 2 proj, fci V)=   %+16.8f Ha, conv = %s" % ((e_extcorr[5]/nsite), extcorr_conv[5]))
+print("E(T-CCSD, 2-site FCI, 0 proj)=           %+16.8f Ha, conv = %s" % ((e_tailor[0]/nsite), tailor_conv[0]))
+print("E(T-CCSD, 2-site FCI, 1 proj)=           %+16.8f Ha, conv = %s" % ((e_tailor[1]/nsite), tailor_conv[1]))
+print("E(T-CCSD, 2-site FCI, 2 proj)=           %+16.8f Ha, conv = %s" % ((e_tailor[2]/nsite), tailor_conv[2]))
+print("E(DT-CCSD, 2-site FCI, 0 proj)=          %+16.8f Ha, conv = %s" % ((e_dtailor[0]/nsite), dtailor_conv[0]))
+print("E(DT-CCSD, 2-site FCI, 1 proj)=          %+16.8f Ha, conv = %s" % ((e_dtailor[1]/nsite), dtailor_conv[1]))
+print("E(DT-CCSD, 2-site FCI, 2 proj)=          %+16.8f Ha, conv = %s" % ((e_dtailor[2]/nsite), dtailor_conv[2]))
