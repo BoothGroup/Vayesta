c30aa81b7fccf57f3c1384accbed9467de7d03b5
Charles Scott <cjcargillscott@gmail.com>
2023-01-19 16:40:58 +0000

    Added test for RIRPA error estimation.

--- vayesta/tests/rpa/test_rirpa_molecule.py
+++ vayesta/tests/rpa/test_rirpa_molecule.py
@@ -15,13 +15,10 @@ class MoleculeRPATest(TestCase):
 
         self.assertAlmostEqual(emb.e_tot, known_values['e_tot'], self.PLACES)
 
-    def _test_mom0(self, rpa_orig, rirpa):
+    def _test_mom0(self, rpa_orig, rirpa_moms):
         """Test that the RPA and RIRPA zeroth moments agree.
         """
-
-        rim0, error_est = rirpa.kernel_moms(3)
-
-        self.assertAlmostEqual(abs(rim0 - rpa_orig.gen_moms(3)).max(), 0.0, self.PLACES)
+        self.assertAlmostEqual(abs(rirpa_moms - rpa_orig.gen_moms(3)).max(), 0.0, self.PLACES)
 
     def test_n2_ccpvdz_dRIRPA(self):
         """Tests for LiH cc-pvdz with dRIRPA.
@@ -34,9 +31,16 @@ class MoleculeRPATest(TestCase):
         self._test_energy(orig_rpa, known_values)
 
         rirpa = rpa.ssRIRPA(testsystems.n2_ccpvdz_df.rhf())
+        rirpa_moms, error_est = rirpa.kernel_moms(3, analytic_lower_bound=True)
 
-        self._test_mom0(orig_rpa, rirpa)
+        self._test_mom0(orig_rpa, rirpa_moms)
 
+    def test_n2_ccpvdz_dRIRPA_error_estimates(self):
+        rirpa = rpa.ssRIRPA(testsystems.n2_ccpvdz_df.rhf())
+        # Use number of points where errors will be meaningful.
+        rirpa_moms, error_est = rirpa.kernel_moms(0, analytic_lower_bound=True, npoints=4)
+        self.assertAlmostEqual(error_est[0], 0.0600120085568549,)
+        self.assertAlmostEqual(error_est[1], 0.0003040334773499559)
 
 if __name__ == '__main__':
     print('Running %s' % __file__)
