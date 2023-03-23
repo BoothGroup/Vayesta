7f731a85ec5aa98d6baf76a22dba8136bd8387ca
Charles Scott <cjcargillscott@gmail.com>
2023-03-23 17:02:29 +0000

    Almost functional UHF implementation of variational embedding.

--- vayesta/misc/variational_embedding/calc_nonorth_couplings.py
+++ vayesta/misc/variational_embedding/calc_nonorth_couplings.py
@@ -90,29 +90,32 @@ def calc_full_couplings(h1e, h2e, ovlp, mo1, mo2, nmo, nocc, nact1, nact2, ncore
 def calc_full_couplings_uhf(h1e, h2e, ovlp, mo1, mo2, nmo, nocc, nact1, nact2, ncore1, ncore2, enuc=0.0):
 
     h1e, h2e, ovlp = owndata(h1e), owndata(h2e), owndata(ovlp)
-    mo1, mo2 = owndata(mo1), owndata(mo2)
+    mo1, mo2 = [owndata(x) for x in mo1], [owndata(x) for x in mo2]
 
     # Can't currently support different size active spaces.
-    assert(nact1 == nact2)
-    assert(ncore1 == ncore2)
-    orbs = wick.wick_orbitals[float, float](nmo, nmo, nocc, mo1, mo2, ovlp, nact1, ncore1)
+    assert(nact1[0] == nact2[0] and nact1[1] == nact2[1])
+    assert(ncore1[0] == ncore2[0] and ncore1[1] == ncore2[1])
+    orbs1 = wick.wick_orbitals[float, float](nmo, nmo, nocc[0], mo1[0], mo2[0], ovlp, nact1[0], ncore1[0])
+    orbs2 = wick.wick_orbitals[float, float](nmo, nmo, nocc[1], mo1[1], mo2[1], ovlp, nact1[1], ncore1[1])
 
-    mb = wick.wick_uscf[float, float, float](orbs, ovlp, enuc)
+    mb = wick.wick_uscf[float, float, float](orbs1, orbs2, ovlp, enuc)
     mb.add_one_body(h1e)
     mb.add_two_body(h2e)
 
-    v1 = utils.fci_bitset_list(nocc - ncore1, nact1)
-    v2 = utils.fci_bitset_list(nocc - ncore2, nact2)
+    v1a = utils.fci_bitset_list(nocc[0] - ncore1[0], nact1[0])
+    v1b = utils.fci_bitset_list(nocc[1] - ncore1[1], nact1[1])
 
-    h = np.zeros((len(v1), len(v1), len(v2), len(v2)))
+    v2a = utils.fci_bitset_list(nocc[0] - ncore2[0], nact2[0])
+    v2b = utils.fci_bitset_list(nocc[1] - ncore2[1], nact2[1])
+
+    h = np.zeros((len(v1a), len(v1b), len(v2a), len(v2b)))
     s = np.zeros_like(h)
-    for i1a in range(len(v1)):
-        for i1b in range(len(v1)):
-            for i2a in range(len(v2)):
-                for i2b in range(len(v2)):
-                    stmp, htmp = mb.evaluate(v1[i1a], v1[i1b], v2[i2a], v2[i2b])
+    for i1a in range(len(v1a)):
+        for i1b in range(len(v1b)):
+            for i2a in range(len(v2a)):
+                for i2b in range(len(v2b)):
+                    stmp, htmp = mb.evaluate(v1a[i1a], v1b[i1b], v2a[i2a], v2b[i2b])
                     h[i1a, i1b, i2a, i2b] = htmp
                     s[i1a, i1b, i2a, i2b] = stmp
 
-    return h.reshape((len(v1)**2, len(v2)**2)), s.reshape((len(v1)**2, len(v2)**2))
-
+    return h.reshape((len(v1a) * len(v1b), len(v2a) * len(v2b))), s.reshape((len(v1a) * len(v1b), len(v2a) * len(v2b)))
--- vayesta/misc/variational_embedding/variational_params.py
+++ vayesta/misc/variational_embedding/variational_params.py
@@ -4,45 +4,31 @@ from pyscf import gto, scf, mcscf, ao2mo, lib
 from vayesta.core.types import RFCI_WaveFunction, SpatialOrbitals
 from .calc_nonorth_couplings import calc_full_couplings, calc_ci_elements, calc_full_couplings_uhf
 
-def optimise_full_varwav(emb, lindep=1e-12):
-    orig_linear = emb.get_e_corr("wf") + emb.e_mf
-    h, s, nstates = gen_full_ham(emb, returnsizes=True)
+def optimise_full_varwav(emb, fs=None, lindep=1e-12, replace_wf=False):
+    fullham, fullovlp = gen_loc_hams(emb, fs)
+    h, s, nstates = to_single_ham(fullham, fullovlp, returnsizes=True)
 
     w, v, seig = lib.linalg_helper.safe_eigh(h, s, lindep=lindep)
 
     ci0 = v[:, 0]
 
     orig_ci = [f.results.wf.ci for f in emb.fragments]
-    orig_ecorr = [f.results.e_corr for f in emb.fragments]
+
     for x in orig_ci:
         print(x)
     i = 0
-    for ns, f in zip(nstates, emb.fragments):
-        if f.sym_parent is None:
-            print("orig:")
-            c = ci0[i:i + ns].reshape(f.results.wf.ci.shape)
-            print("new:")
-            f.results.wf.ci = c
-            ci = f.results.wf.as_cisd(c0=1.0)
-            print("eris:",f._eris)
-            es, ed, f.results.e_corr = f.get_fragment_energy(ci.c1, ci.c2, eris=f._eris)
+    if replace_wf:
+        for ns, f in zip(nstates, emb.fragments):
+            if f.sym_parent is None:
+                c = ci0[i:i + ns].reshape(f.results.wf.ci.shape)
+                # Copy to avoid wiping any other copies.
+                f.results.wf = f.results.wf.copy()
+                f.results.wf.ci = c
             i += ns
-            print("After replacement:")
 
-    elinear = emb.get_e_corr("wf") +emb.e_mf
+    return w[0]
 
-    for f, ci, ec in zip(emb.fragments, orig_ci, orig_ecorr):
-        if f.sym_parent is None:
-            f.results.wf.ci = ci
-            ci = f.results.wf.as_cisd(c0=1.0)
-            es, ed, f.results.e_corr = f.get_fragment_energy(ci.c1, ci.c2, eris=f._eris)
-
-    print("Energy diff without projection: {:16.12e}".format(orig_linear - emb.get_e_corr("wf") - emb.e_mf))
-    print(elinear, emb.get_e_corr("wf") + emb.e_mf)
-    return w[0], elinear
-
-def gen_full_ham(emb, fs=None, returnsizes=False):
-
+def gen_loc_hams(emb, fs=None):
     if fs is None:
         fs = emb.fragments
 
@@ -50,32 +36,40 @@ def gen_full_ham(emb, fs=None, returnsizes=False):
     mos = [x.cluster.c_total for x in fs]
 
     h1e = emb.get_hcore()
-    h2e = emb.get_eris_array(np.eye(emb.nao)).reshape((emb.nao**2, emb.nao**2))
     ovlp = emb.get_ovlp()
-    nmo, nocc = emb.nao, emb.nocc
 
-    nact = [x.mo.norb for x in wfs]
-    ncore = [emb.nocc - x.mo.nocc for x in wfs]
+    if emb.is_rhf:
+        h2e = emb.get_eris_array(np.eye(emb.nao)).reshape((emb.nao**2, emb.nao**2))
+        nmo, nocc = emb.nao, emb.nocc
+        nact = [x.mo.norb for x in wfs]
+        ncore = [emb.nocc - x.mo.nocc for x in wfs]
 
+        coupl_func = calc_full_couplings
 
-
+    else:
+        h2e = emb.get_eris_array(np.eye(emb.nao)).reshape((emb.nao**2, emb.nao**2))
+        nmo, nocc = emb.nao, emb.nocc
+        nact = [x.mo.norb for x in wfs]
+        ncore = [[y - z for y,z in zip(emb.nocc, x.mo.nocc)] for x in wfs]
+        coupl_func = calc_full_couplings_uhf
     fullham = np.zeros((len(fs), len(fs)), dtype=object)
     fullovlp = np.zeros_like(fullham)
     for i in range(len(fs)):
         for j in range(i, len(fs)):
-            fullham[i, j], fullovlp[i, j] = calc_full_couplings(h1e, h2e, ovlp, mos[i], mos[j], nmo, nocc, nact[i], nact[j], ncore[i], ncore[j], enuc=emb.e_nuc)
-            #
-            #testham, testovlp = calc_full_couplings_uhf(h1e, h2e, ovlp, mos[i], mos[j], nmo, nocc, nact[i], nact[j], ncore[i], ncore[j], enuc=emb.e_nuc)
-            #print("!!!!!!",abs(fullham[i,j] - testham).max(), abs(fullovlp[i,j] - testovlp).max())
+            fullham[i, j], fullovlp[i, j] = coupl_func(h1e, h2e, ovlp, mos[i], mos[j], nmo, nocc, nact[i],
+                                                                    nact[j], ncore[i], ncore[j], enuc=emb.e_nuc)
             fullham[j, i] = fullham[i, j].T
             fullovlp[j, i] = fullovlp[i, j].T
+    return fullham, fullovlp
+
+
+def to_single_ham(fullham, fullovlp, returnsizes=False):
 
     nstates = sum([x.shape[1] for x in fullham[0]])
     h = np.zeros((nstates, nstates))
     s = np.zeros_like(h)
 
     ix = 0
-
     for (hx, sx) in zip(fullham, fullovlp):
         iy = 0
         for (hloc, sloc) in zip(hx, sx):
