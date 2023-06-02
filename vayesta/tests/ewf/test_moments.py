003c9aa83e5c5cf9c7c27694dff9558bd820db7f
Basil Ibrahim <basil.ibrahim@kcl.ac.uk>
2023-06-02 17:17:13 +0100

    Add CCSD moment test

--- vayesta/tests/ewf/test_moments.py
+++ vayesta/tests/ewf/test_moments.py
@@ -42,6 +42,40 @@ class Test_RFCI(TestCase):
             self.assertTrue(np.allclose(ip, fci_ip))
             self.assertTrue(np.allclose(ea, fci_ea))
 
+
+class Test_RCCSD(TestCase):
+
+    def test(self):
+
+        #RHF
+        mf = testsystems.water_ccpvdz.rhf()
+
+        try:
+            from dyson.expressions import CCSD
+        except ImportError:
+            vayesta.logger.info("Could not import Dyson. Skipping FCI moment tests.")
+            return
+
+        fci = CCSD["1h"](mf)
+        fci_ip = fci.build_gf_moments(4)
+
+        fci = CCSD["1p"](mf)
+        fci_ea = fci.build_gf_moments(4)
+
+        #Full bath EWF
+        ewf = vayesta.ewf.EWF(mf, bath_type='full', solver_options=dict(n_moments=(4,4)), solver='CCSD')
+        ewf.kernel()
+
+        for f in ewf.fragments:
+            ip, ea = f.results.moms 
+
+            cx = f.get_overlap('mo|cluster')
+            ip = np.einsum('pP,qQ,nPQ->npq', cx, cx, ip)
+            ea = np.einsum('pP,qQ,nPQ->npq', cx, cx, ea)
+
+            self.assertTrue(np.allclose(ip, fci_ip))
+            self.assertTrue(np.allclose(ea, fci_ea))
+
 if __name__ == '__main__':
     print("Running %s" % __file__)
     unittest.main()
