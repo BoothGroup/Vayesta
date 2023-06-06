c3abd545e2f396fe4c2cf7a009562c6ff498c416
Basil Ibrahim <basil.ibrahim@kcl.ac.uk>
2023-06-06 16:40:42 +0100

    Compare norm

--- vayesta/tests/ewf/test_moments.py
+++ vayesta/tests/ewf/test_moments.py
@@ -56,11 +56,11 @@ class Test_RCCSD(TestCase):
             vayesta.log.info("Could not import Dyson. Skipping FCI moment tests.")
             return
 
-        fci = CCSD["1h"](mf)
-        fci_ip = fci.build_gf_moments(4)
+        cc = CCSD["1h"](mf)
+        cc_ip = cc.build_gf_moments(4)
 
-        fci = CCSD["1p"](mf)
-        fci_ea = fci.build_gf_moments(4)
+        cc = CCSD["1p"](mf)
+        cc_ea = cc.build_gf_moments(4)
 
         #Full bath EWF
         ewf = vayesta.ewf.EWF(mf, bath_type='full', solver_options=dict(n_moments=(4,4)), solver='CCSD')
@@ -73,8 +73,13 @@ class Test_RCCSD(TestCase):
             ip = np.einsum('pP,qQ,nPQ->npq', cx, cx, ip)
             ea = np.einsum('pP,qQ,nPQ->npq', cx, cx, ea)
 
-            self.assertTrue(np.allclose(ip, fci_ip))
-            self.assertTrue(np.allclose(ea, fci_ea))
+            print('Norm %f'%np.linalg.norm(ip- cc_ip))
+            print('IP %f'%np.linalg.norm(ea- cc_ea))
+            self.assertAlmostEqual(np.linalg.norm(ip- cc_ip), 0.0)
+            self.assertAlmostEqual(np.linalg.norm(ea- cc_ea), 0.0)
+
+            # self.assertTrue(np.allclose(ip, cc_ip))
+            # self.assertTrue(np.allclose(ea, cc_ea))
 
 if __name__ == '__main__':
     print("Running %s" % __file__)
