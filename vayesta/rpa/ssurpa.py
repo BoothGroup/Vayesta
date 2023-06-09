b6c92903095b27df552389c3bd0d88bd12ce3cbf
Charles Scott <cjcargillscott@gmail.com>
2023-06-09 14:37:03 +0100

    Restored missing import in ssURPA.

--- vayesta/rpa/ssurpa.py
+++ vayesta/rpa/ssurpa.py
@@ -3,7 +3,7 @@ import numpy as np
 import scipy.linalg
 
 from timeit import default_timer as timer
-from vayesta.core.util import dot, time_string
+from vayesta.core.util import dot, time_string, einsum
 
 
 class ssURPA(ssRPA):
