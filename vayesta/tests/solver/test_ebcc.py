e0d7b40597f16e5ebfa599b5d7933de772fc8a3b
Charles Scott <cjcargillscott@gmail.com>
2023-08-03 16:23:06 +0100

    Initial set of ebCC tests; not currently all passing.

---
+++ vayesta/tests/solver/test_ebcc.py
@@ -0,0 +1,57 @@
+import pytest
+
+import pyscf
+import pyscf.cc
+
+import vayesta
+import vayesta.ewf
+
+from vayesta.tests.common import TestCase
+from vayesta.tests import testsystems
+
+# Note that ebCC currently doesn't support density fitting, so we'll just test non-DF results.
+
+@pytest.mark.fast
+class TestCCSD(TestCase):
+    try:
+        import ebcc
+    except ImportError:
+        pytest.skip("Requires cvxpy")
+
+    def _test(self, key):
+        mf = getattr(getattr(testsystems, key[0]), key[1])()
+
+        emb = vayesta.ewf.EWF(mf, solver=f'EB{key[2]}', bath_options=dict(bathtype='full'),
+                              solver_options=dict(solve_lambda=False))
+        emb.kernel()
+        import ebcc
+
+        cc = ebcc.REBCC(mf, ansatz=key[-1])
+        cc.kernel()
+
+        self.assertAlmostEqual(emb.e_corr, cc.e_corr)
+        self.assertAlmostEqual(emb.e_tot, cc.e_tot)
+
+    def test_rccsd_h2(self):
+        return self._test(('h2_ccpvdz', 'rhf', 'CCSD'))
+
+    def test_rccsd_water_631g(self):
+        return self._test(('water_631g', 'rhf', 'CCSD'))
+
+    def test_uccsd_water_631g(self):
+        return self._test(('water_631g', 'uhf', 'CCSD'))
+
+    def test_uccsd_water_cation_631g(self):
+        return self._test(('water_cation_631g', 'uhf', 'CCSD'))
+
+    def test_rccsdt_water_631g(self):
+        return self._test(('water_631g', 'rhf', 'CCSDT'))
+
+    def test_uccsdt_water_cation_631g(self):
+        return self._test(('water_cation_631g', 'uhf', 'CCSDT'))
+
+    def test_rccsdtprime_water_sto3g(self):
+        return self._test(('water_sto3g', 'rhf', "CCSDt'", 'CCSDT'))
+
+    def test_uccsdtprime_water_sto3g(self):
+        return self._test(('water_sto3g', 'uhf', "CCSDt'", 'CCSDT'))
