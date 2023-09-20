d1c0a4e35d727cc11f6dcb695b66a79c12ecbc8f
Max Nusspickel <max.nusspickel@gmail.com>
2023-09-20 21:41:46 +0100

    Adds tests for example files

---
+++ vayesta/tests/test_examples.py
@@ -0,0 +1,21 @@
+import sys
+from pathlib import Path
+import importlib
+
+import pytest
+
+
+example_path = Path(__file__).parent.parent.parent / 'examples'
+examples_files = list(example_path.glob('**/*.py'))
+
+
+@pytest.fixture(params=examples_files[:4], ids=lambda x: Path(x).name)
+def example_file(request):
+    return request.param
+
+
+def test_example(example_file):
+    spec = importlib.util.spec_from_file_location(example_file.name, str(example_file))
+    example = importlib.util.module_from_spec(spec)
+    sys.modules["module.name"] = example
+    spec.loader.exec_module(example)
