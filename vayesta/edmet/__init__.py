14fc95ab066c0053327e8c8e5b099ba600a04c79
Basil Ibrahim <basil.ibrahim@kcl.ac.uk>
2024-03-01 21:19:47 +0000

    Fix bug for kUHF and consistent conversion of ROHF to UHF

--- vayesta/edmet/__init__.py
+++ vayesta/edmet/__init__.py
@@ -15,9 +15,9 @@ log = logging.getLogger(__name__)
 
 def EDMET(mf, *args, **kwargs):
     """Determine restricted or unrestricted by inspection of mean-field object"""
-    if isinstance(mf, pyscf.scf.uhf.UHF):
+    if isinstance(mf, pyscf.scf.uhf.UHF) or isinstance(mf, pyscf.pbc.scf.kuhf.KUHF):
         return UEDMET(mf, *args, **kwargs)
-    elif isinstance(mf, pyscf.scf.rohf.ROHF):
+    elif isinstance(mf, pyscf.scf.rohf.ROHF) or isinstance(mf, pyscf.pbc.scf.krohf.KROHF):
         log.warning("Converting ROHF reference to UHF")
         return UEMET(mf.to_uhf(), *args, **kwargs)
     return REDMET(mf, *args, **kwargs)
