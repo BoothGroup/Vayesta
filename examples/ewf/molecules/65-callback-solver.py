4fd07f2aaf8982099dc06fd5fa9e50ec35e1ca0c
Basil Ibrahim <basil.ibrahim@kcl.ac.uk>
2024-03-29 20:58:20 +0000

    Add example

--- examples/ewf/molecules/65-callback-solver.py
+++ examples/ewf/molecules/65-callback-solver.py
@@ -6,24 +6,19 @@ import pyscf.fci
 import vayesta
 import vayesta.ewf
 from vayesta.misc.molecules import ring
-from vayesta.core.types import Orbitals, FCI_WaveFunction, CISD_WaveFunction
-import h5py
 
-
+# User defined FCI solver - takes pyscf mf as input and returns RDMs
 def solver(mf):
     h1e = mf.get_hcore()
     h2e = mf._eri
     norb = mf.mo_coeff.shape[-1]
     nelec = mf.mol.nelec
-    energy, ci_vec = pyscf.fci.direct_spin0.kernel(h1e, h2e, norb, nelec, conv_tol=1.e-14)
-    
-    dm1, dm2 = pyscf.fci.direct_spin0.make_rdm12(ci_vec, norb, nelec)
-
-    results = dict(dm1=dm1, dm2=dm2, converged=True, energy=energy, ci_vec=ci_vec)
-
+    energy, civec = pyscf.fci.direct_spin0.kernel(h1e, h2e, norb, nelec, conv_tol=1.e-14)
+    dm1, dm2 = pyscf.fci.direct_spin0.make_rdm12(civec, norb, nelec)
+    results = dict(dm1=dm1, dm2=dm2, converged=True)
     return results
 
-natom = 2
+natom = 10
 mol = pyscf.gto.Mole()
 mol.atom = ring("H", natom, 1.5)
 mol.basis = "sto-3g"
@@ -35,31 +30,27 @@ mol.build()
 # Hartree-Fock
 mf = pyscf.scf.RHF(mol)
 mf.kernel()
-print('Mean-Field (Hartree--Fock) energy = {}'.format(mf.e_tot))
-
 
 # Vayesta options
 use_sym = True
-nfrag = 1 # When using symmetry, we have to specify the number of atoms in the fragment in this case.
-#bath_opts = dict(bathtype="full")  # This is a complete bath space (should be exact)
-bath_opts = dict(bathtype="dmet")   # This is the smallest bath size
-# bath_opts = dict(bathtype='mp2', threshold=1.e-6)
+nfrag = 1 
+bath_opts = dict(bathtype="dmet")   
 
-# Run vayesta for comparison with FCI solver
+# Run vayesta with user defined solver
 emb = vayesta.ewf.EWF(mf, solver="CALLBACK",  energy_functional='dm', bath_options=bath_opts, solver_options=dict(callback=solver))
 # Set up fragments
 with emb.iao_fragmentation() as f:
     if use_sym:
         # Add rotational symmetry
-        # Set order of rotation (2: 180 degrees, 3: 120 degrees, 4: 90: degrees,...),
-        # axis along which to rotate and center of rotation (default units for axis and center are Angstroms):
         with f.rotational_symmetry(order=natom//nfrag, axis=[0, 0, 1]):
             f.add_atomic_fragment(range(nfrag))
     else:
         # Add all atoms as separate fragments 
         f.add_all_atomic_fragments()
 emb.kernel()
-print("DMET energy: %s"%emb.get_dmet_energy(part_cumulant=False, approx_cumulant=False))
-print("DMET energy (part-cumulant): %s"%emb.get_dmet_energy(part_cumulant=True, approx_cumulant=False))
+
+print("Hartree-Fock energy          : %s"%mf.e_tot)
+print("DMET energy                  : %s"%emb.get_dmet_energy(part_cumulant=False, approx_cumulant=False))
+print("DMET energy   (part-cumulant): %s"%emb.get_dmet_energy(part_cumulant=True, approx_cumulant=False))
 print("DMET energy (approx-cumulant): %s"%emb.get_dmet_energy(part_cumulant=True, approx_cumulant=True))
-print("ERROR: %s"%emb.get_dmet_energy(part_cumulant=False, approx_cumulant=True))
+
