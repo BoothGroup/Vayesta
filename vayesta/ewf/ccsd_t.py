9dce0a585eef6ddd11667f60ab6bc107549bf36f
Basil Ibrahim <basil.ibrahim@kcl.ac.uk>
2024-07-29 18:56:38 +0100

    Option to calculate (T) contribution

--- vayesta/ewf/ccsd_t.py
+++ vayesta/ewf/ccsd_t.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pyscf.lib
 
-def calc_fragment_ccsd_t_energy(fragment, t1, t2, eris=None, project='w'):
+def calc_fragment_ccsd_t_energy(fragment, t1=None, t2=None, eris=None, project='w'):
     """
     Calculates a fragment CCSD(T) energy contribution.
 
@@ -12,6 +12,11 @@ def calc_fragment_ccsd_t_energy(fragment, t1, t2, eris=None, project='w'):
 
     einsum = pyscf.lib.einsum
 
+    if t1 is None:
+        t1 = fragment.results.wf.as_ccsd().t1
+    if t2 is None:
+        t2 = fragment.results.wf.as_ccsd().t2
+
     t1T = t1.T
     t2T = t2.transpose(2,3,0,1)
 
@@ -20,6 +25,8 @@ def calc_fragment_ccsd_t_energy(fragment, t1, t2, eris=None, project='w'):
     e_occ, e_vir = mo_e[:nocc], mo_e[nocc:]
     eijk = pyscf.lib.direct_sum('i,j,k->ijk', e_occ, e_occ, e_occ)
 
+
+    # NOTE: These transpositions are for indexing the outer loop, they are NOT the vvov, vooo and vvoo blocks of the ERIS
     if eris is not None:
         eris_vvov = eris.get_ovvv().conj().transpose(1,3,0,2)
         eris_vooo = np.asarray(eris.ovoo).conj().transpose(1,0,2,3)
@@ -29,6 +36,7 @@ def calc_fragment_ccsd_t_energy(fragment, t1, t2, eris=None, project='w'):
         eris_vooo = fragment.hamil.get_eris_bare(block='ovoo').conj().transpose(1,0,2,3)
         eris_vvoo = fragment.hamil.get_eris_bare(block='ovov').conj().transpose(1,3,0,2)
 
+        
     fvo = fragment.hamil.get_fock(with_vext=True)[nocc:,:nocc]
     def get_w(a, b, c):
         w = einsum('if,fkj->ijk', eris_vvov[a,b], t2T[c,:])
