20e4f6604bc1281899fd7a87588dd700e18d10a3
Basil Ibrahim <basil.ibrahim@kcl.ac.uk>
2024-09-25 19:51:51 +0100

    Add ref value for 2D test

--- vayesta/tests/ewf/test_h2_pbc.py
+++ vayesta/tests/ewf/test_h2_pbc.py
@@ -341,7 +341,7 @@ class Test_CCSD_2D(Test_CCSD):
         cls.ref_values = {
             ("e_corr", -1): cls.cc.e_corr / nk,
             ("e_tot", -1): cls.cc.e_tot / nk,
-            ("e_ccsd_t", -1): cls.cc.uccsd_t() / nk,
+            ("e_ccsd_t", -1): cls.cc.ccsd_t() / nk,
             ("e_corr", 1e-3): -0.019820060226576966,
         }
         cls.ref_values[("e_tot", 1e-3)] = cls.mf.e_tot + cls.ref_values[("e_corr", 1e-3)]
