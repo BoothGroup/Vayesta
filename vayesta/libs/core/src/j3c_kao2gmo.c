456e01f651258171127355864055d6f0fe6984b2
basilib <basil.ibrahim@kcl.ac.uk>
2025-03-24 20:19:41 -0400

    Update j3c_kao2gmo.c

--- vayesta/libs/core/src/j3c_kao2gmo.c
+++ vayesta/libs/core/src/j3c_kao2gmo.c
@@ -12,13 +12,18 @@
 #include <math.h>
 #include <assert.h>
 
+#include "mkl.h"
+#ifndef __INTEL_MKL__
+    #include "cblas.h"
+#endif
+
 #if defined _OPENMP
 #include "omp.h"
 #else
 #define omp_get_thread_num() 0
 #define omp_get_num_threads() 1
 #endif
-#include "mkl.h"
+
 
 /* NOT USED ANYMORE */
 int64_t j3c_k2gamma(
